8973253dbfea17d01c1181ed46602f1e00e97b69
jose.lausuch <jose.lausuch@ericsson.com>
2015-09-30 15:29:04 +0200

    Dockerfile for Functest + script to trigger Functest automatically JIRA: FUNCTEST-29
    Change-Id: I230631e43e5f7e14938b35903ecec1b17db0f88a
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ testcases/docker/start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# 
+# Author: Jose Lausuch (szilard.cserey@ericsson.com)
+#
+# Installs the Functest framework and run the tests automatically
+#
+
+# SCHELETON 
+#1) fetch_os_creds
+#2) source creds
+#3) python $WORKSPACE/testcases/config_functest.py --debug $WORKSPACE/ start
+#4) run vPing
+#5) run ODL tests
+#6) run Rally bench 
+#7) run Tempest
+#
+
