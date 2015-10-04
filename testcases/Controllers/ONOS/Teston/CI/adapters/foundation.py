26202454ab57a4094ebab379dca3498ab7455eff
Qinglong Lan <lanqinglong@huawei.com>
2015-10-04 12:52:13 +0800

    Write a simple adapter in functest project to run TESTON JIRA:FUNCTEST-46
    Change-Id: I146ec926da6cbbd2535d0997326a13bd545f42c6
    Signed-off-by: Qinglong Lan <lanqinglong@huawei.com>

---
+++ testcases/Controllers/ONOS/Teston/CI/adapters/foundation.py
@@ -0,0 +1,32 @@
+"""
+Description:
+    This file include basis functions
+    lanqinglong@huawei.com
+"""
+
+import logging
+import os
+import time
+
+class foundation:
+
+    def __init__(self):
+        self.dir = os.path.join( os.getcwd(), 'log' )
+
+    def log (self, loginfo):
+        """
+        Record log in log directory for deploying test environment
+        parameters:
+        loginfo(input): record info 
+        """
+        filename = time.strftime( '%Y-%m-%d-%H-%M-%S' ) + '.log'
+        filepath = os.path.join( self.dir, filename )
+        logging.basicConfig( level=logging.INFO,
+                format = '%(asctime)s %(filename)s:%(message)s',
+                datefmt = '%d %b %Y %H:%M:%S',
+                filename = filepath,
+                filemode = 'w')
+        filelog = logging.FileHandler( filepath )
+        logging.getLogger( 'Functest' ).addHandler( filelog )
+        print loginfo
+        logging.info(loginfo)
