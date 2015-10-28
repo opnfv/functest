1ab3d5a313d465a2941a77780e9742d241a78367
jose.lausuch <jose.lausuch@ericsson.com>
2015-10-28 16:41:17 +0100

    Added reset to rally commit ID in prepare_env.sh
    Change-Id: I0d662409bfde353c8efd2fa3fd8cacf9c849428b
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- docker/prepare_env.sh
+++ docker/prepare_env.sh
@@ -104,6 +104,10 @@ if [ $offline == false ]; then
     cd ${RALLY_REPO_DIR}
     info "Rally repo: pulling to latest..."
     git pull
+    if [ ${RALLY_COMMIT} != "latest" ]; then
+        info "Rally repo: given commit is ${RALLY_COMMIT}. Reseting..."
+        git reset --hard ${RALLY_COMMIT}
+    fi
 
     info "Updating vIMS test repository...."
     cd ${VIMS_REPO_DIR}
