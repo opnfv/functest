adb631a4007b26131b49288d576c9dc87c45dbb4
meimei <meimei@huawei.com>
2015-11-17 22:00:40 -0800

    [functest] modify functest prepare scripts to adapt compass
    JIRA: COMPASS-157
    
    Change-Id: I81f1f41ff7a10e9bcea1cb46b9dc2e2f8e169bae
    Signed-off-by: meimei <meimei@huawei.com>

--- docker/prepare_env.sh
+++ docker/prepare_env.sh
@@ -61,10 +61,11 @@ fi
 info "Checking environment variables INSTALLER_TYPE and INSTALLER_IP"
 if [ -z ${INSTALLER_TYPE} ]; then
     error "Environment variable 'INSTALLER_TYPE' is not defined."
-elif [ "${INSTALLER_TYPE}" != "fuel" ] && [ "${INSTALLER_TYPE}" != "foreman" ]; then
+elif [[ ${INSTALLER_TYPE} =~ ^(fuel|foreman|compass|apex|juju)$ ]]; then
+    info "INSTALLER_TYPE env variable found: ${INSTALLER_TYPE}"
+else
     error "Invalid environment variable INSTALLER_TYPE=${INSTALLER_TYPE}"
 fi
-info "INSTALLER_TYPE env variable found: ${INSTALLER_TYPE}"
 
 if [ -z ${INSTALLER_IP} ]; then
     error "Environment variable 'INSTALLER_IP' is not defined."
