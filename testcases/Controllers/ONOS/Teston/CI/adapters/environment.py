cc27777f84ea018cc2c0487a4fc3ff0c3b6d358f
Morgan Richomme <morgan.richomme@orange.com>
2016-01-19 11:48:39 +0100

    Add mention to apache 2 license in Functest files and remove unused files
    Change-Id: Iaf67792ad61351d0573c8d92e5d78f5f7a6e86ff
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

--- testcases/Controllers/ONOS/Teston/CI/adapters/environment.py
+++ testcases/Controllers/ONOS/Teston/CI/adapters/environment.py
@@ -6,6 +6,13 @@ Description:
             Set user name/password
             Onos-push-keys and so on
     lanqinglong@huawei.com
+
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+#
 """
 
 import os
