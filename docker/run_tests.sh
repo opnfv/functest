e6bcc951ad6ff395fa342f6cfec5729de7ac8c6c
Morgan Richomme <morgan.richomme@orange.com>
2016-01-19 11:48:39 +0100

    Add mention to apache 2 license in Functest files and remove unused files
    Change-Id: Iaf67792ad61351d0573c8d92e5d78f5f7a6e86ff
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>
    (cherry picked from commit cc27777f84ea018cc2c0487a4fc3ff0c3b6d358f)

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -2,9 +2,15 @@
 
 #
 # Author: Jose Lausuch (jose.lausuch@ericsson.com)
-#
+#         Morgan Richomme (morgan.richomme@orange.com)
 # Installs the Functest framework within the Docker container
 # and run the tests automatically
+#
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 
 usage="Script to trigger the tests automatically.
--- testcases/vIMS/vIMS.md
+++
@@ -1,3 +0,0 @@
-# vIMS README
-
-
