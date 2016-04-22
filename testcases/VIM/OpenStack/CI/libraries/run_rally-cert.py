ade54b8ec63fc674cfd7d7e0a893b7e34f6b44b1
Morgan Richomme <morgan.richomme@orange.com>
2016-04-22 11:18:36 +0200

    Affect version field in results for all the testcases
    Previously version was scenario
    
    JIRA: FUNCTEST-225
    
    Change-Id: Ia86f83dbe98ad5f9dbced60c46e57f58eccba7e9
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

--- testcases/VIM/OpenStack/CI/libraries/run_rally-cert.py
+++ testcases/VIM/OpenStack/CI/libraries/run_rally-cert.py
@@ -136,8 +136,7 @@ def push_results_to_db(case, payload, criteria):
     url = TEST_DB + "/results"
     installer = functest_utils.get_installer_type(logger)
     scenario = functest_utils.get_scenario(logger)
-    # Until we found a way to manage version, use scenario
-    version = scenario
+    version = functest_utils.get_version(logger)
     pod_name = functest_utils.get_pod_name(logger)
 
     # evalutate success criteria
