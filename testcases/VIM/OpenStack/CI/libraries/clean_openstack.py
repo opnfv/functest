65c7d973975621c7eb2d660bbdcb0d94bafd6f3a
vitikkan <viktor.tikkanen@nokia.com>
2016-04-26 11:23:08 +0300

    Corrected logging.Formatter parameters
    Signed-off-by: vitikkan <viktor.tikkanen@nokia.com>

--- testcases/VIM/OpenStack/CI/libraries/clean_openstack.py
+++ testcases/VIM/OpenStack/CI/libraries/clean_openstack.py
@@ -48,8 +48,8 @@ if args.debug:
 else:
     ch.setLevel(logging.INFO)
 
-formatter = logging.Formatter('% (asctime)s - % (name)s - '
-                              '% (levelname)s - % (message)s')
+formatter = logging.Formatter('%(asctime)s - %(name)s - '
+                              '%(levelname)s - %(message)s')
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
