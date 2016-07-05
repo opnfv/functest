70054edf920b00951479864c084b149f6294375d
Manuel Buil <manuel.buil@ericsson.com>
2016-07-05 17:14:51 +0200

    Add tacker-client in the functest docker image
    Tacker-client is required for the SFC test cases
    
    Change-Id: I21b1e47c890fb45899f6f83b6583b0ab032662d1
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

---
+++ testcases/features/sfc/tacker_client_install.sh
@@ -0,0 +1,38 @@
+MYDIR=$(dirname $(readlink -f "$0"))
+CLIENT=$(echo python-python-tackerclient_*_all.deb)
+CLIREPO="tacker-client"
+
+# Function checks whether a python egg is available, if not, installs
+function chkPPkg () {
+    PKG="$1"
+    IPPACK=$(python - <<'____EOF'
+import pip
+from os.path import join
+for package in pip.get_installed_distributions():
+    print(package.location)
+    print(join(package.location, *package._get_metadata("top_level.txt")))
+____EOF
+)
+    echo "$IPPACK" | grep -q "$PKG"
+    if [ $? -ne 0 ];then
+        pip install "$PKG"
+    fi
+}
+
+function envSetup () {
+    apt-get install -y python-all debhelper
+    chkPPkg stdeb
+    chkCrudini
+}
+
+# Function installs python-tackerclient from github
+function deployTackerClient() {
+    cd $MYDIR
+    git clone -b 'SFC_refactor' https://github.com/trozet/python-tackerclient.git $CLIREPO
+    cd $CLIREPO
+    python setup.py --command-packages=stdeb.command bdist_deb
+    cd "deb_dist"
+    CLIENT=$(echo python-python-tackerclient_*_all.deb)
+    cp $CLIENT $MYDIR
+    dpkg -i "${MYDIR}/${CLIENT}"
+}
