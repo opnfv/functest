60c65d31ec90200af20834f724c66ca5cd4a3210
Manuel Buil <manuel.buil@ericsson.com>
2016-07-27 18:03:55 +0200

    Scripts added to enable the SFC-ODL test in CI
    We need to install Tacker in the server before running the test cases
    
    Change-Id: Id7daee80627bc71c335fcb3efa5d389594641b65
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

---
+++ testcases/features/sfc/set-up-tacker.sh
@@ -0,0 +1,28 @@
+#wget https://gerrit.opnfv.org/gerrit/#/c/15065/5/prototypes/sfc_tacker/poc.tacker-up.sh
+#bash poc.tacker-up.sh
+
+touch delete.sh
+
+cat <<EOF > delete.sh
+tacker sfc-classifier-delete red_http
+tacker sfc-classifier-delete blue_ssh
+tacker sfc-classifier-delete red_ssh
+tacker sfc-classifier-delete blue_http
+tacker sfc-delete red
+tacker sfc-delete blue
+tacker vnf-delete testVNF1
+tacker vnf-delete testVNF2
+tacker vnfd-delete test-vnfd1
+tacker vnfd-delete test-vnfd2
+#openstack stack delete sfc --y
+heat stack-delete sfc
+#openstack stack delete sfc_test1 --y
+heat stack-delete sfc_test1
+#openstack stack delete sfc_test2 --y
+heat stack-delete sfc_test2
+EOF
+
+chmod +x delete.sh
+
+#source tackerc
+#openstack flavor create custom --ram 1500 --disk 10 --public
