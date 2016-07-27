a9e7f209bb78253f79cf1863704b94242ffe3e6d
Manuel Buil <manuel.buil@ericsson.com>
2016-07-27 18:03:55 +0200

    Scripts added to enable the SFC-ODL test in CI
    We need to install Tacker in the server before running the test cases
    
    Change-Id: Id7daee80627bc71c335fcb3efa5d389594641b65
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

---
+++ testcases/features/sfc/server_presetup_CI.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+ssh_options='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
+BASEDIR=`dirname $0`
+INSTALLER_IP=${INSTALLER_IP:-10.20.0.2}
+
+ip=`sshpass -p r00tme ssh $ssh_options root@10.20.0.2 'fuel node'|grep controller|awk '{print $10}' | head -1`
+echo $ip
+sshpass -p r00tme scp set-up-tacker.sh $INSTALLER_IP:/root
+sshpass -p r00tme ssh $ssh_options root@$INSTALLER_IP 'scp set-up-tacker.sh '"$ip"':/root'
+sshpass -p r00tme ssh $ssh_options root@$INSTALLER_IP 'ssh root@'"$ip"' bash set-up-tacker.sh'
+sshpass -p r00tme ssh $ssh_options root@$INSTALLER_IP 'scp '"$ip"':/root/tackerc '"$BASEDIR"''
+sshpass -p r00tme scp 10.20.0.2:/root/tackerc .
