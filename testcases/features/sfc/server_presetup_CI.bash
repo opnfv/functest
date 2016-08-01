608da53af832ee8d4badead7acde66bd399cca84
Manuel Buil <manuel.buil@ericsson.com>
2016-08-01 10:09:10 +0200

    Several bug fixes in ODL-SFC functest testcase
    JIRA: SFC-42
    
    - SSH options added so that the ssh key is not needed
    - IPTABLES change done before contacting nova, neutron...
    - Fix a problem in the CI test (we need to CD the BASEDIR)
    
    Change-Id: Id63c02a614bf31e5a33637c2bf95270bd1399ff8
    Signed-off-by: Manuel Buil <manuel.buil@ericsson.com>

--- testcases/features/sfc/server_presetup_CI.bash
+++ testcases/features/sfc/server_presetup_CI.bash
@@ -4,6 +4,7 @@ ssh_options='-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no'
 BASEDIR=`dirname $0`
 INSTALLER_IP=${INSTALLER_IP:-10.20.0.2}
 
+pushd $BASEDIR
 ip=`sshpass -p r00tme ssh $ssh_options root@${INSTALLER_IP} 'fuel node'|grep controller|awk '{print $10}' | head -1`
 echo $ip
 sshpass -p r00tme scp $ssh_options set-up-tacker.sh ${INSTALLER_IP}:/root
