35bff578b3a592fbb3e991a58415a23a60494a97
Luke Hinds <lukehinds@gmail.com>
2016-08-12 15:14:47 +0100

    Provides connection check for package download.
    JIRA: FUNCTEST-388
    
    Performs simple connection test, using socket from std python
    library. If no connection can be made, socket will timeout and
    gracefully move to trying the next compute / control node.
    
    Change-Id: I0048d74d607c8824688c73791c9646f9e9186756
    Signed-off-by: Luke Hinds <lukehinds@gmail.com>

---
+++ testcases/security_scan/scripts/internet_check.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2016 Red Hat
+# Luke Hinds (lhinds@redhat.com)
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Performs simple connection check, falls to default timeout of 10 seconds
+
+import socket
+TEST_HOST = "google.com"
+def is_connected():
+  try:
+    host = socket.gethostbyname(TEST_HOST)
+    sock = socket.create_connection((host, 80), 2)
+    return True
+  except:
+     return False
+print is_connected()
