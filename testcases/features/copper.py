34947aa5d4ea991f20c94c5216a2f6a01cbfa37f
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-08-31 10:37:15 +0800

    delete useless FUNCTEST_REPO variable
    FUNCTEST_REPO appears in lots of feature testcases, but never be used, delete it clean the code
    
    JIRA: FUNCTEST-448
    
    Change-Id: I4a9a10eac29e09b481563ed2c33f4122dd9ee3de
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>
    (cherry picked from commit 834f81f0ad708b930f66aec1fd8b36e2cfb8b32b)

--- testcases/features/copper.py
+++ testcases/features/copper.py
@@ -32,7 +32,6 @@ with open(os.environ["CONFIG_FUNCTEST_YAML"]) as f:
     functest_yaml = yaml.safe_load(f)
 
 dirs = functest_yaml.get('general').get('directories')
-FUNCTEST_REPO = dirs.get('dir_repo_functest')
 COPPER_REPO = dirs.get('dir_repo_copper')
 
 logger = ft_logger.Logger("copper").getLogger()
