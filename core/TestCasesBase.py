d516b95eb674d5f7ccdb259df80cc73123aa4840
Cédric Ollivier <cedric.ollivier@orange.com>
2016-10-10 17:24:55 +0200

    Add a parent class for test cases.
    It adds TestCasesBase on which test cases can be based.
    It also adds the related unit tests.
    
    JIRA: FUNCTEST-353
    
    Change-Id: Ib2aeb24ce9d43e6384e8b38bf57ee78dd726345a
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ core/TestCasesBase.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import os
+
+import functest.utils.functest_logger as ft_logger
+import functest.utils.functest_utils as ft_utils
+
+
+class TestCasesBase(object):
+
+    EX_OK = os.EX_OK
+    EX_RUN_ERROR = os.EX_SOFTWARE
+    EX_PUSH_TO_DB_ERROR = os.EX_SOFTWARE - 1
+
+    logger = ft_logger.Logger(__name__).getLogger()
+
+    project = "functest"
+
+    def __init__(self):
+        self.details = {}
+        self.case_name = ""
+        self.criteria = ""
+        self.start_time = ""
+        self.stop_time = ""
+
+    def run(self, **kwargs):
+        self.logger.error("Run must be implemented")
+        return TestCasesBase.EX_RUN_ERROR
+
+    def push_to_db(self):
+        try:
+            assert self.case_name
+            assert self.criteria
+            assert self.start_time
+            assert self.stop_time
+            if ft_utils.push_results_to_db(
+                    TestCasesBase.project, self.case_name, self.start_time,
+                    self.stop_time, self.criteria, self.details):
+                self.logger.info("The results were successfully pushed to DB")
+                return TestCasesBase.EX_OK
+            else:
+                self.logger.error("The results cannot be pushed to DB")
+                return TestCasesBase.EX_PUSH_TO_DB_ERROR
+        except Exception:
+            self.logger.exception("The results cannot be pushed to DB")
+            return TestCasesBase.EX_PUSH_TO_DB_ERROR
