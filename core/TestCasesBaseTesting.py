51da8aedbca51977c6bb21abc925981dafcc91ae
Cédric Ollivier <cedric.ollivier@orange.com>
2016-10-10 17:24:55 +0200

    Add a parent class for test cases.
    It add TestCasesBase on which test cases can be based.
    It also adds the related unit tests.
    
    Change-Id: Ib2aeb24ce9d43e6384e8b38bf57ee78dd726345a
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ core/TestCasesBaseTesting.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import logging
+import mock
+import unittest
+
+import TestCasesBase
+
+
+class TestCasesBaseTesting(unittest.TestCase):
+
+    logging.disable(logging.CRITICAL)
+
+    def setUp(self):
+        self.test = TestCasesBase.TestCasesBase()
+        self.test.case_name = "odl"
+        self.test.criteria = "100"
+        self.test.start_time = "1"
+        self.test.stop_time = "2"
+
+    def test_run_unimplemented(self):
+        self.assertEqual(self.test.run(),
+                         TestCasesBase.TestCasesBase.EX_RUN_ERROR)
+
+    def _test_missing_attribute(self):
+        with mock.patch('functest.utils.functest_utils.push_results_to_db',
+                        return_value=False) as mock_function:
+            self.assertEqual(self.test.push_to_db(),
+                             TestCasesBase.TestCasesBase.EX_PUSH_TO_DB_ERROR)
+            mock_function.assert_not_called()
+
+    def test_missing_case_name(self):
+        self.test.case_name = None
+        self._test_missing_attribute()
+
+    def test_missing_criteria(self):
+        self.test.criteria = None
+        self._test_missing_attribute()
+
+    def test_missing_start_time(self):
+        self.test.start_time = None
+        self._test_missing_attribute()
+
+    def test_missing_stop_time(self):
+        self.test.stop_time = None
+        self._test_missing_attribute()
+
+    def test_missing_details(self):
+        with mock.patch('functest.utils.functest_utils.push_results_to_db',
+                        return_value=True) as mock_function:
+            self.assertEqual(self.test.push_to_db(),
+                             TestCasesBase.TestCasesBase.EX_OK)
+            mock_function.assert_called_once_with(
+                'functest', 'odl', '1', '2', '100', {})
+
+    def test_push_to_db_failed(self):
+        self.test.details = { "Hello":  "World" }
+        with mock.patch('functest.utils.functest_utils.push_results_to_db',
+                        return_value=False) as mock_function:
+            self.assertEqual(self.test.push_to_db(),
+                             TestCasesBase.TestCasesBase.EX_PUSH_TO_DB_ERROR)
+            mock_function.assert_called_once_with(
+                'functest', 'odl', '1', '2', '100', { "Hello":  "World" })
+
+    def test_push_to_db(self):
+        self.test.details = { "Hello":  "World" }
+        with mock.patch('functest.utils.functest_utils.push_results_to_db',
+                        return_value=True) as mock_function:
+            self.assertEqual(self.test.push_to_db(),
+                             TestCasesBase.TestCasesBase.EX_OK)
+            mock_function.assert_called_once_with(
+                'functest', 'odl', '1', '2', '100', { "Hello":  "World" })
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
