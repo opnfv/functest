87ddf690c47d38659d9b126bb56aa7de18d22ef8
Morgan Richomme <morgan.richomme@orange.com>
2016-10-17 16:14:50 +0200

    Add unittests dir
    JIRA: FUNCTEST-336
    
    Change-Id: Icd958552fafb4e8dc58f9008586bcaf33862ccd4
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

---
+++ setup.py
@@ -0,0 +1,24 @@
+##############################################################################
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name="functest",
+    version="master",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+    },
+    url="https://www.opnfv.org",
+    install_requires=["coverage==4.1",
+                      "mock==1.3.0",
+                      "nose==1.3.7"],
+    entry_points={
+    }
+)
