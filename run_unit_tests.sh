590261ed85e51d0d8a04101e59cd2b29711619f3
Morgan Richomme <morgan.richomme@orange.com>
2016-10-17 16:14:50 +0200

    Add unittests dir
    JIRA: FUNCTEST-336
    
    Change-Id: Icd958552fafb4e8dc58f9008586bcaf33862ccd4
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

---
+++ run_unit_tests.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+
+# date +"%T"
+echo "Running unit tests..."
+cd .
+
+# Set some en variables if not already set
+if [ -z "$CONFIG_FUNCTEST_YAML" ]; then
+    echo "set CONFIG_FUNCTEST_YAML (not found in env variables)"
+    export CONFIG_FUNCTEST_YAML="./ci/config_functest.yaml"
+fi 
+
+if [ -z "$repos_dir" ]; then
+    echo "set repos_dir (not found in env variables)"
+    export repos_dir=".."
+fi 
+
+# start vitual env
+virtualenv ./functest_venv
+source ./functest_venv/bin/activate
+
+# install python packages
+easy_install -U setuptools
+easy_install -U pip
+pip install -r docker/requirements.pip
+pip install -e .
+
+# unit tests
+nosetests --with-xunit \
+         --with-coverage \
+         --cover-package=functest\
+         --cover-xml \
+         unit_tests
+
+deactivate
+# date +"%T"
