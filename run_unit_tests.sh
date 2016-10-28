7592187c78a1cd4c33707d30192699c01c2ceb91
Morgan Richomme <morgan.richomme@orange.com>
2016-10-28 12:32:18 +0200

    Add exit return code for unit tests
    JIRA: FUNCTEST-336
    
    Change-Id: Ib88256a167c003805d480f57c218c37d8d5962d8
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -3,17 +3,47 @@ set -o errexit
 set -o pipefail
 
 echo "Running unit tests..."
-cd .
+
+if [ -z $WORKSPACE ]
+then
+    WORKSPACE="$HOME"
+fi
+
+# Create log dir if needed (TODO clean that...)
+if [ ! -d "/home/opnfv/functest/results" ]
+then
+    sudo mkdir -p /home/opnfv/functest/results
+fi
+
+if [ ! -d "/home/opnfv/functest/results/odl" ]
+then
+    sudo mkdir -p /home/opnfv/functest/results/odl
+fi
+
+# as we import the module from the home repo
+# and in jenkins the name is different
+# functest(verify-master != functest
+# make some ugly adjustments...
+cd $WORKSPACE
+export PYTHONPATH="${PYTHONPATH}:$WORKSPACE"
+cd ..
+
+if [ ! -d "./functest" ]
+then
+ln -s functest-verify-master functest
+fi
 
 # start vitual env
-virtualenv ./functest_venv
-source ./functest_venv/bin/activate
+virtualenv $WORKSPACE/functest_venv
+source $WORKSPACE/functest_venv/bin/activate
 
 # install python packages
 easy_install -U setuptools
 easy_install -U pip
-pip install -r docker/requirements.pip
-pip install -e .
+pip install -r $WORKSPACE/docker/requirements.pip
+pip install -e $WORKSPACE
+
+python $WORKSPACE/setup.py develop
 
 # unit tests
 nosetests --with-xunit \
@@ -22,5 +52,11 @@ nosetests --with-xunit \
          --cover-package=functest.testcases.Controllers.ODL.OpenDaylightTesting \
          --cover-xml \
          unit_tests
+rc=$?
 
 deactivate
+
+# clean
+sudo rm -rf /home/opnfv/functest/results
+
+exit $rc
