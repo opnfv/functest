4f185fab546fd7661685f47db3cb04dc12145642
Morgan Richomme <morgan.richomme@orange.com>
2016-10-28 12:32:18 +0200

    Add exit return code for unit tests
    JIRA: FUNCTEST-336
    
    Change-Id: Ib88256a167c003805d480f57c218c37d8d5962d8
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -3,17 +3,36 @@ set -o errexit
 set -o pipefail
 
 echo "Running unit tests..."
-cd .
+
+if [ -z $WORKSPACE ]
+then
+    WORKSPACE="$HOME"
+fi
 
 # start vitual env
-virtualenv ./functest_venv
-source ./functest_venv/bin/activate
+virtualenv $WORKSPACE/functest_venv
+source $WORKSPACE/functest_venv/bin/activate
 
 # install python packages
 easy_install -U setuptools
 easy_install -U pip
 pip install -r docker/requirements.pip
 pip install -e .
+
+python ./setup.py develop
+
+echo " ************** "
+which nosetests
+echo " ************** "
+echo $PWD
+echo " ************** "
+echo " ************** "
+echo $PYTHONPATH
+echo " ************** "
+which python
+echo " ************** "
+
+
 
 # unit tests
 nosetests --with-xunit \
@@ -22,5 +41,8 @@ nosetests --with-xunit \
          --cover-package=functest.testcases.Controllers.ODL.OpenDaylightTesting \
          --cover-xml \
          unit_tests
+rc=$?
 
 deactivate
+
+exit $rc
