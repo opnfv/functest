8f96715f30536e435a7cd53ad02657ac7d3f0e6c
jose.lausuch <jose.lausuch@ericsson.com>
2016-11-23 11:24:49 +0100

    Conform SFC test case to FeatureBase
    JIRA: FUNCTEST-550
    
    Change-Id: I46853d1ba943a54b39b760c34c29c2b4c3b43d63
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ functest/opnfv_tests/features/odl_sfc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+# Copyright (c) 2016 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+import functest.core.feature_base as base
+
+
+class OpenDaylightSFC(base.FeatureBase):
+
+    def __init__(self):
+        super(OpenDaylightSFC, self).__init__(project='sfc',
+                                              case='functest-odl-sfc"',
+                                              repo='dir_repo_sfc')
+        self.cmd = 'cd %s/tests/functest && python ./run_tests.py' % self.repo
