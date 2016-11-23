b4f98d7af1daa77f95f2960ef1fd0d4a79fbdafa
jose.lausuch <jose.lausuch@ericsson.com>
2016-11-23 11:24:49 +0100

    DO NOT MERGE. Conform SFC test case to TestCaseBase
    JIRA: FUNCTEST-550
    
    We still need to move the logic of calling  prepare_odl_sfc.py
    and sourcing tackerc in the same sfc test case.
    
    Change-Id: I46853d1ba943a54b39b760c34c29c2b4c3b43d63
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- functest/ci/exec_test.sh
+++ functest/ci/exec_test.sh
@@ -156,12 +156,6 @@ function run_test(){
                 $clean_flag -s -m feature_multisite $report \
                 -c ${FUNCTEST_TEST_DIR}/OpenStack/tempest/tempest_multisite.conf
         ;;
-        "odl-sfc")
-            ODL_SFC_DIR=${repos_dir}/sfc/tests/functest/odl-sfc
-            FUNCTEST_REPO_DIR=${FUNCTEST_REPO_DIR} python ${ODL_SFC_DIR}/prepare_odl_sfc.py || exit $?
-            source ${ODL_SFC_DIR}/tackerc
-            python ${ODL_SFC_DIR}/sfc.py $report
-        ;;
         "parser")
             python ${FUNCTEST_TEST_DIR}/vnf/vRNC/parser.py $report
         ;;
