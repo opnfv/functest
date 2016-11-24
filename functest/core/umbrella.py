a79a20dc659a2dda0af4a2f708796dee9e241f2c
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-11-24 10:32:53 +0800

    abstract umbrella part to make integration code simpler
    implement an abstract class Umbrella, and take parser for instance
    
    JIRA: FUNCTEST-618
    
    Change-Id: Iadf71ad49abd901b9659cb71135fc902a3b616d1
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

---
+++ functest/core/umbrella.py
@@ -0,0 +1,65 @@
+import time
+
+import TestCasesBase
+import functest.utils.functest_utils as ft_utils
+import functest.utils.functest_logger as ft_logger
+
+
+class Umbrella(TestCasesBase.TestCasesBase):
+    def __init__(self, project='functest', case='', repo='', cmd=''):
+        super(Umbrella, self).__init__()
+        self.project_name = project
+        self.case_name = case
+        self.cmd = cmd
+        self.repo = self.get_conf('general.directories.{}'.format(repo))
+        self.result_file = self.get_result_file()
+        self.logger = ft_logger.Logger(project).getLogger()
+
+    def run(self, **kwargs):
+        self.prepare()
+        self.start_time = time.time()
+        ret = ft_utils.execute_command(self.cmd, output_file=self.result_file)
+        self.stop_time = time.time()
+        self.post()
+        self.parse_results(ret)
+        self.log_results()
+
+        if ret == 0:
+            return TestCasesBase.TestCasesBase.EX_OK
+        else:
+            return TestCasesBase.TestCasesBase.EX_RUN_ERROR
+
+    def prepare(self, **kwargs):
+        pass
+
+    def post(self, **kwargs):
+        pass
+
+    def parse_results(self, ret):
+        def get_criteria_value():
+            criteria = ft_utils.get_criteria_by_test(self.project_name)
+            return criteria.split('==')[1].strip()
+
+        self.criteria = 'FAIL'
+        if str(ret) == get_criteria_value():
+            self.criteria = 'PASS'
+
+        self.details = {
+            'timestart': self.start_time,
+            'duration': round(self.stop_time - self.start_time, 1),
+            'status': self.criteria,
+        }
+
+    def get_result_file(self):
+        dir = self.get_conf('general.directories.dir_results')
+        return "{}/{}.log".format(dir, self.project_name)
+
+    def log_results(self):
+        ft_utils.logger_test_results(self.project_name,
+                                     self.case_name,
+                                     self.criteria,
+                                     self.details)
+
+    @staticmethod
+    def get_conf(parameter):
+        return ft_utils.get_functest_config(parameter)
--- functest/opnfv_tests/vnf/vRNC/parser.py
+++ functest/opnfv_tests/vnf/vRNC/parser.py
@@ -14,64 +14,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import sys
-import time
 
-import argparse
-
-import functest.core.TestCasesBase as base
-import functest.utils.functest_logger as ft_logger
-import functest.utils.functest_utils as ft_utils
+import functest.core.umbrella as umbrella
 
 
-class Parser(base.TestCasesBase):
+class Parser(umbrella.Umbrella):
     def __init__(self):
-        super(Parser, self).__init__()
-        self.project_name = "parser"
-        self.case_name = "parser-basics"
-        self.parser_repo = self.get_conf('general.directories.dir_repo_parser')
-        self.results_dir = self.get_conf('general.directories.dir_results')
-        self.logger = ft_logger.Logger("parser").getLogger()
-        self.log_file = self.results_dir + '/parser.log'
-
-    def run(self, **kwargs):
-        cmd = 'cd %s/tests && ./functest_run.sh' % self.parser_repo
-
-        self.start_time = time.time()
-        ret = ft_utils.execute_command(cmd,
-                                       info=True,
-                                       output_file=self.log_file)
-        self.stop_time = time.time()
-
-        self.criteria, details = ft_utils.check_test_result(self.project_name,
-                                                            ret,
-                                                            self.start_time,
-                                                            self.stop_time)
-
-        ft_utils.logger_test_results(self.project_name,
-                                     self.case_name,
-                                     self.criteria,
-                                     details)
-
-        return ret
-
-    @staticmethod
-    def get_conf(parameter):
-        return ft_utils.get_functest_config(parameter)
-
-
-if __name__ == '__main__':
-    args_parser = argparse.ArgumentParser()
-    args_parser.add_argument("-r", "--report",
-                             help="Create json result file",
-                             action="store_true")
-    args = vars(args_parser.parse_args())
-    parser = Parser()
-    try:
-        result = parser.run(**args)
-        if result != base.TestCasesBase.EX_OK:
-            sys.exit(result)
-        if args['report']:
-            sys.exit(parser.push_to_db())
-    except Exception:
-        sys.exit(base.TestCasesBase.EX_RUN_ERROR)
+        super(Parser, self).__init__(project='parser',
+                                     case='parser-basics',
+                                     repo='dir_repo_parser')
+        self.cmd = 'cd %s/tests && ./functest_run.sh' % self.repo
