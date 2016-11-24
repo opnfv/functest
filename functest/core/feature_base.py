12bdbb7a0971b48ebee742a47033ee55aeab1419
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-11-24 10:32:53 +0800

    abstract umbrella part to make integration code simpler
    implement an abstract class Umbrella, and take parser for instance
    
    JIRA: FUNCTEST-618
    
    Change-Id: Iadf71ad49abd901b9659cb71135fc902a3b616d1
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

---
+++ functest/core/feature_base.py
@@ -0,0 +1,67 @@
+import time
+
+import TestCasesBase
+import functest.utils.functest_utils as ft_utils
+import functest.utils.functest_logger as ft_logger
+
+
+class FeatureBase(TestCasesBase.TestCasesBase):
+    def __init__(self, project='functest', case='', repo='', cmd=''):
+        super(FeatureBase, self).__init__()
+        self.project_name = project
+        self.case_name = case
+        self.cmd = cmd
+        self.repo = self.get_conf('general.directories.{}'.format(repo))
+        self.result_file = self.get_result_file()
+        self.logger = ft_logger.Logger(project).getLogger()
+
+    def run(self, **kwargs):
+        self.prepare()
+        self.start_time = time.time()
+        ret = ft_utils.execute_command(self.cmd, output_file=self.result_file)
+        self.stop_time = time.time()
+        self.post()
+        exit_code = self.parse_results(ret)
+        self.log_results()
+        return exit_code
+
+    def prepare(self, **kwargs):
+        pass
+
+    def post(self, **kwargs):
+        pass
+
+    def parse_results(self, ret):
+        def get_criteria_value():
+            criteria = ft_utils.get_criteria_by_test(self.project_name)
+            return criteria.split('==')[1].strip()
+
+        exit_code = TestCasesBase.TestCasesBase.EX_OK
+        criteria_v = get_criteria_value()
+        duration = round(self.stop_time - self.start_time, 1)
+        if str(ret) == criteria_v and duration > 1:
+            self.logger.info("{} OK".format(self.project_name))
+            self.criteria = 'PASS'
+        elif str(ret) == criteria_v and duration <= 1:
+            self.logger.info("{} SKIPPED".format(self.project_name))
+            exit_code = TestCasesBase.TestCasesBase.EX_SKIP
+        else:
+            self.logger.info("{} FAILED".format(self.project_name))
+            exit_code = TestCasesBase.TestCasesBase.EX_TEST_FAIL
+            self.criteria = "FAIL"
+
+        return exit_code
+
+    def get_result_file(self):
+        dir = self.get_conf('general.directories.dir_results')
+        return "{}/{}.log".format(dir, self.project_name)
+
+    def log_results(self):
+        ft_utils.logger_test_results(self.project_name,
+                                     self.case_name,
+                                     self.criteria,
+                                     self.details)
+
+    @staticmethod
+    def get_conf(parameter):
+        return ft_utils.get_functest_config(parameter)
