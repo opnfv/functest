89d8dda3d6da8d33bc52c36b5ccbf3a345c75568
jose.lausuch <jose.lausuch@ericsson.com>
2016-12-21 14:51:30 +0100

    Adapt SDNVPN tests using Featurebase
    Change-Id: I8be4bb72ba1d3b7eb2ba6e1edccf61d4186e07c6
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- functest/opnfv_tests/features/sdnvpn.py
+++ functest/opnfv_tests/features/sdnvpn.py
@@ -7,70 +7,14 @@
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 #
+import functest.core.feature_base as base
 
 
-import argparse
-import os
-import sys
-import time
-
-import functest.core.testcase_base as testcase_base
-import functest.utils.functest_constants as ft_constants
-import functest.utils.functest_logger as ft_logger
-import functest.utils.functest_utils as ft_utils
-
-
-class SdnVpnTests(testcase_base.TestcaseBase):
-    SDNVPN_REPO_TESTS = os.path.join(
-        ft_constants.SDNVPN_REPO_DIR, "tests/functest")
-    logger = ft_logger.Logger("sdnvpn").getLogger()
+class SdnVpnTests(base.FeatureBase):
 
     def __init__(self):
-        super(SdnVpnTests, self).__init__()
-        self.project_name = "sdnvpn"
-        self.case_name = "bgpvpn"
-
-    def main(self, **kwargs):
-        os.chdir(self.SDNVPN_REPO_TESTS)
-        cmd = 'run_tests.py'
-        log_file = os.path.join(
-            ft_constants.FUNCTEST_RESULTS_DIR, "sdnvpn.log")
-        start_time = time.time()
-
-        ret = ft_utils.execute_command(cmd,
-                                       output_file=log_file)
-
-        stop_time = time.time()
-        if ret == 0:
-            self.logger.info("%s OK" % self.case_name)
-            status = 'PASS'
-        else:
-            self.logger.info("%s FAILED" % self.case_name)
-            status = "FAIL"
-
-        # report status only if tests run (FAIL OR PASS)
-        self.criteria = status
-        self.start_time = start_time
-        self.stop_time = stop_time
-        self.details = {}
-
-    def run(self):
-        kwargs = {}
-        return self.main(**kwargs)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--report",
-                        help="Create json result file",
-                        action="store_true")
-    args = vars(parser.parse_args())
-    sdnvpn = SdnVpnTests()
-    try:
-        result = sdnvpn.main(**args)
-        if result != testcase_base.TestcaseBase.EX_OK:
-            sys.exit(result)
-        if args['report']:
-            sys.exit(sdnvpn.push_to_db())
-    except Exception:
-        sys.exit(testcase_base.TestcaseBase.EX_RUN_ERROR)
+        super(SdnVpnTests, self).__init__(project='sdnvpn',
+                                          case='bgpvpn',
+                                          repo='dir_repo_sdnvpn')
+        dir_sfc_functest = '{}/sdnvpn/test/functest'.format(self.repo)
+        self.cmd = 'cd %s && python ./run_tests.py' % dir_sfc_functest
