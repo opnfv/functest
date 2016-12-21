468fc5b9fca0c5c90a43aef372c139278c853e6d
wu.zhihui <wu.zhihui1@zte.com.cn>
2016-12-21 09:49:19 +0800

    [WIP]write test results to a local file
    Write test result to a file or push it to DB depends
    on the value format of test_db_url which is defined in
    config_functest.yaml.
    If test_db_url is a url, e.g. http:/// or https:///,
    then push result to DB.
    If test_db_url is a file location, e.g. file:///,
    then write results to a file with json data.
    
    Meanwhile, test_db_url can be set by os envrion value
    "RESULT_STORE".
    
    This patch needs some test. Don't merge.
    
    JIRA: FUNCTEST-657
    
    Change-Id: I579087cd2c24d61a79142b5d67003fb486b6c723
    Signed-off-by: wu.zhihui <wu.zhihui1@zte.com.cn>

--- functest/core/testcase_base.py
+++ functest/core/testcase_base.py
@@ -11,6 +11,7 @@ import os
 
 import functest.utils.functest_logger as ft_logger
 import functest.utils.functest_utils as ft_utils
+from functest.utils.constants import CONST
 
 
 class TestcaseBase(object):
@@ -19,6 +20,7 @@ class TestcaseBase(object):
     EX_RUN_ERROR = os.EX_SOFTWARE
     EX_PUSH_TO_DB_ERROR = os.EX_SOFTWARE - 1
     EX_TESTCASE_FAILED = os.EX_SOFTWARE - 2
+    EX_WRITE_TO_FILE_ERROR = os.EX_SOFTWARE - 3
 
     logger = ft_logger.Logger(__name__).getLogger()
 
@@ -43,21 +45,44 @@ class TestcaseBase(object):
         self.logger.error("Run must be implemented")
         return TestcaseBase.EX_RUN_ERROR
 
-    def push_to_db(self):
+    def publish_report(self):
+        if os.environ['RESULTS']:
+            CONST.results_test_db_url = os.environ['RESULTS']
+
         try:
             assert self.project_name
             assert self.case_name
             assert self.criteria
             assert self.start_time
             assert self.stop_time
-            if ft_utils.push_results_to_db(
-                    self.project_name, self.case_name, self.start_time,
-                    self.stop_time, self.criteria, self.details):
-                self.logger.info("The results were successfully pushed to DB")
-                return TestcaseBase.EX_OK
+            if CONST.results_test_db_url.lower().startswith("http:///") or \
+                    CONST.results_test_db_url.lower().startswith("https:///"):
+                self.push_to_db()
+            elif CONST.results_test_db_url.lower().startswith("file:///"):
+                self.write_to_file()
             else:
-                self.logger.error("The results cannot be pushed to DB")
-                return TestcaseBase.EX_PUSH_TO_DB_ERROR
+                self.logger.error("Please give a valid parameter for -r")
+                return TestcaseBase.EX_RUN_ERROR
         except Exception:
-            self.logger.exception("The results cannot be pushed to DB")
+            self.logger.exception("The results cannot be stored")
+            return TestcaseBase.EX_RUN_ERROR
+
+    def write_to_file(self, path):
+        if ft_utils.write_results_to_file(
+                self.project_name, self.case_name, self.start_time,
+                self.stop_time, self.criteria, self.details):
+            self.logger.info("The results were successfully written to a file")
+            return TestcaseBase.EX_OK
+        else:
+            self.logger.error("wrtie results to a file failed")
+            return TestcaseBase.EX_WRITE_TO_FILE_ERROR
+
+    def push_to_db(self):
+        if ft_utils.push_results_to_db(
+                self.project_name, self.case_name, self.start_time,
+                self.stop_time, self.criteria, self.details):
+            self.logger.info("The results were successfully pushed to DB")
+            return TestcaseBase.EX_OK
+        else:
+            self.logger.error("The results cannot be pushed to DB")
             return TestcaseBase.EX_PUSH_TO_DB_ERROR
