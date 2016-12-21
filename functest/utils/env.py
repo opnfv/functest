f99d8e9d8ec2ac803ebf7a61766b48674b7577e5
SerenaFeng <feng.xiaowei@zte.com.cn>
2016-12-21 15:42:54 +0800

    refactor Parser project using new constants provider
    JIRA: FUNCTEST-673
    
    Change-Id: Ia37fb5a08c8fbc4a8acb666565ea887f9ae48b83
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- functest/utils/env.py
+++ functest/utils/env.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 default_envs = {
     'NODE_NAME': 'unknown_pod',
@@ -19,6 +20,20 @@ class Environment(object):
         for k, v in default_envs.iteritems():
             if k not in os.environ:
                 self.__setattr__(k, v)
+        self._set_ci_run()
+        self._set_ci_loop()
+
+    def _set_ci_run(self):
+        if self.BUILD_TAG:
+            self.IS_CI_RUN = True
+        else:
+            self.IS_CI_RUN = False
+
+    def _set_ci_loop(self):
+        if self.BUILD_TAG and re.search("daily", self.BUILD_TAG):
+            self.CI_LOOP = "daily"
+        else:
+            self.CI_LOOP = "weekly"
 
 
 ENV = Environment()
