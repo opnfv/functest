96df70da90250b375b73aee4793238a15a5ba977
helenyao <yaohelan@huawei.com>
2017-01-05 01:57:01 -0500

    Leverage logging config and refactor the logger
    JIRA: FUNCTEST-690
    
    The detailed benefits are listed in the JIRA description.
    
    1. Refactored the functest_logger to make use of configuration file
    2. Avoided configuring the logging every time a logger is initialized
    3. Applied separate logging configuration fo unit test by removing
    saving log to file
    
    Change-Id: I6e27eaba727ae6d704f0301f15359dc3ea7b2f97
    Signed-off-by: helenyao <yaohelan@huawei.com>

--- functest/utils/functest_logger.py
+++ functest/utils/functest_logger.py
@@ -20,34 +20,51 @@
 #  logger = fl.Logger("script_name").getLogger()
 #  logger.info("message to be shown with - INFO - ")
 #  logger.debug("message to be shown with - DEBUG -")
-
 import logging
+import logging.config
 import os
+
+import json
+
+from functest.utils.constants import CONST
+
+logger = logging.getLogger(__name__)
+
+
+def is_debug():
+    if CONST.CI_DEBUG and CONST.CI_DEBUG.lower() == "true":
+        return True
+    return False
+
+
+stream_level = logging.INFO
+if is_debug():
+    stream_level = logging.DEBUG
+
+
+def setup_logging(default_path=CONST.dir_functest_logging_cfg,
+                  default_level=logging.INFO,
+                  env_key='LOG_CFG'):
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+            if (config['handlers'] and
+                    config['handlers']['console']):
+                config['handlers']['console']['level'] = stream_level
+            logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=default_level)
+
+setup_logging()
 
 
 class Logger:
     def __init__(self, logger_name):
-
-        CI_DEBUG = os.getenv('CI_DEBUG')
-
         self.logger = logging.getLogger(logger_name)
-        self.logger.propagate = 0
-        self.logger.setLevel(logging.DEBUG)
-
-        ch = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - '
-                                      '%(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        if CI_DEBUG is not None and CI_DEBUG.lower() == "true":
-            ch.setLevel(logging.DEBUG)
-        else:
-            ch.setLevel(logging.INFO)
-        self.logger.addHandler(ch)
-
-        hdlr = logging.FileHandler('/home/opnfv/functest/results/functest.log')
-        hdlr.setFormatter(formatter)
-        hdlr.setLevel(logging.DEBUG)
-        self.logger.addHandler(hdlr)
 
     def getLogger(self):
         return self.logger
