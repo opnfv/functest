a4dc0ca4589c7187eb3b81a26cf2348dd0ef39b8
helenyao <yaohelan@huawei.com>
2017-01-26 03:43:41 -0500

    Return the config as dict instead of separate item
    Change-Id: I0202afecf3e0a593247a45f817d8d0f279b03e23
    Signed-off-by: helenyao <yaohelan@huawei.com>

---
+++ functest/utils/constants_new.py
@@ -0,0 +1,38 @@
+import config_new as config
+import env
+
+
+class Constants(object):
+    def __init__(self):
+        for attr_n, attr_v in config.CONF.__dict__.iteritems():
+            self.__setattr__(attr_n, attr_v)
+        for env_n, env_v in env.ENV.__dict__.iteritems():
+            self.__setattr__(env_n, env_v)
+
+    def __getattr__(self, key):
+        dict_key = key.split('_')
+        if dict_key and len(dict_key) >= 1:
+            dict_one_key = dict_key.pop(0)
+            value = config.CONF.functest_yaml.get(dict_one_key)
+            if not dict_key:
+                if value is None:
+                    raise ValueError("The key '%s' is not found" % key)
+                return value
+            for element in dict_key:
+                value = value.get(element)
+                if value is None:
+                    raise ValueError("The key '%s' is not found" % key)
+            return value
+
+
+CONST = Constants()
+
+if __name__ == '__main__':
+    # print CONST.__dict__
+    print CONST.NODE_NAME
+    print CONST.general_dir_tempestCases
+    print CONST.tempest_identity_tenantName
+    print CONST.vnf_aaa
+    print CONST.ONOS
+    print CONST.ONOS_environment
+    print CONST.ONOS_environment_OCT
