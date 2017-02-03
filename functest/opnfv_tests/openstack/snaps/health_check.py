cae6958108c290ef87f613ac62ac138ecd47b1b5
spisarski <s.pisarski@cablelabs.com>
2017-02-03 13:29:20 -0700

    Integrating PyTest contained in SNAPS as a replacement to the old bash healthcheck.
    JIRA: FUNCTEST-570
    
    Change-Id: Ia90dc152d48144c9c53967d5c30256389793ce91
    Signed-off-by: spisarski <s.pisarski@cablelabs.com>

---
+++ functest/opnfv_tests/openstack/snaps/health_check.py
@@ -0,0 +1,34 @@
+# Copyright (c) 2015 All rights reserved
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import unittest
+
+from snaps.openstack.tests.os_source_file_test import OSIntegrationTestCase
+from snaps.openstack.tests.create_instance_tests import SimpleHealthCheck
+
+from functest.core.pytest_suite_runner import PyTestSuiteRunner
+from functest.opnfv_tests.openstack.snaps import snaps_utils
+from functest.utils.constants import CONST
+
+
+class HealthCheck(PyTestSuiteRunner):
+    """
+    This test executes the SNAPS Python Test case SimpleHealthCheck which
+    creates a VM with a single port with an IPv4 address that is assigned by
+    DHCP. This test then validates the expected IP with the actual
+    """
+    def __init__(self):
+        super(HealthCheck, self).__init__()
+
+        self.suite = unittest.TestSuite()
+        self.case_name = "snaps_health_check"
+        ext_net_name = snaps_utils.get_ext_net_name()
+
+        self.suite.addTest(
+            OSIntegrationTestCase.parameterize(
+                SimpleHealthCheck, CONST.openstack_creds, ext_net_name,
+                use_keystone=CONST.snaps_use_keystone))
