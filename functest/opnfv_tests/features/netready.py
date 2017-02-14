6958a4f26ff731145d461279b7f1cd3d564b5867
jose.lausuch <jose.lausuch@ericsson.com>
2017-02-14 14:06:32 +0100

    [WIP] Integration of NETREADY test case
    Change-Id: I523a05e0f0fbf5971ab7d10a177d793bcff60421
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ functest/opnfv_tests/features/netready.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+
+#
+import functest.core.feature_base as base
+
+
+class GluonVping(base.FeatureBase):
+
+    def __init__(self):
+        super(GluonVping, self).__init__(project='netready',
+                                         case='gluon_vping',
+                                         repo='dir_repo_netready')
+        dir_netready_functest = '{}/netready/test/functest'.format(self.repo)
+        self.cmd = 'cd %s && python ./gluon-test-suite.py' % dir_netready_functest
