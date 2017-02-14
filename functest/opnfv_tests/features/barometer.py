e68503ddbf354263d960e717b64f320517eac416
jose.lausuch <jose.lausuch@ericsson.com>
2017-02-14 13:37:35 +0100

    Integrate BAROMETER feature test case
    Change-Id: Id734553dffe32fdc9a0befc3f4c0e29e5d56fc61
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ functest/opnfv_tests/features/barometer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import functest.core.feature_base as base
+
+
+class Barometercollectd(base.FeatureBase):
+
+    def __init__(self):
+        super(Barometercollectd, self).__init__(project='barometer',
+                                                case='barometercollectd',
+                                                repo='dir_repo_barometer')
+        self.cmd = 'cd %s && python ./tests/barometer_collectd.py' % self.repo
