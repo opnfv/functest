664a436e7da2abf73b21965cefd0b27858374141
jose.lausuch <jose.lausuch@ericsson.com>
2017-02-14 13:37:35 +0100

    Integrate BAROMETER feature test case
    Also, add a executor method in VNFBase
    to allow re-write it for some cases
    where the feature is another python
    script instead of shell script.
    
    Change-Id: Id734553dffe32fdc9a0befc3f4c0e29e5d56fc61
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

---
+++ functest/opnfv_tests/features/barometer.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+
+import time
+
+import functest.core.feature_base as base
+import functest.utils.functest_logger as ft_logger
+import functest.utils.functest_utils as functest_utils
+
+from baro_tests import collectd
+
+
+class BarometerCollectd(base.FeatureBase):
+    '''
+    Class for executing barometercollectd testcase.
+    '''
+
+    def __init__(self):
+        super(BarometerCollectd, self).__init__(project='barometer',
+                                                case='barometercollectd',
+                                                repo='dir_repo_barometer')
+        self.logger = ft_logger.Logger("BarometerCollectd").getLogger()
+
+    def execute(self):
+        return collectd.main(self.logger)
