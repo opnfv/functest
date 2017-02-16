0a3a3a3142d1761a96b221b55edfc2e459a4b3a3
Cristina Pauna <cristina.pauna@enea.com>
2017-02-16 12:22:21 +0200

    Add aarch64 custom config
    In order to run functest on an aarch64 POD, some custom configuration
    is needed. Up until now that custom configuration was put on the arm
    jenkins slave. With this commit it is integrated in functest project.
    
    1. In order to identify if the POD is aarch64 or x86 function
       get_arch() from https://gerrit.opnfv.org/gerrit/#/c/29099
       is used
    
    2. Changes to config_functest.yaml are made via a new patch file
       config_aarch64_patch.yaml which is applied for all scenarios.
       Function patch_config_file() is modified to be more generic. It
       now receives a file as parameter and merges any type of patch file
    
    3. The aarch64 cirros images are downloaded into the data directory.
       Functest can be run from both an aarch64 or a x86 jenkins slave
       in order to test an aarch64 POD. So the images are downloded in both
       dockerfiles
    
    4. Other custom config needs to be added to /etc/rally/rally.conf
       in the docker. The custom config is set in rally_aarch64_patch.conf
       and inserted in the needed file before the rally environment is
       set up
    
    JIRA: ARMBAND-60
    
    Change-Id: I8e460cdf02d297c259cbbd9c05e7983367fd2bb9
    Signed-off-by: Cristina Pauna <cristina.pauna@enea.com>

---
+++ docker/add_images.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# This script downloads the images that are used for testing
+# and places them in the functest docker image
+
+CIRROS_REPO_URL=http://download.cirros-cloud.net
+CIRROS_AARCH64_TAG=daily/20160722
+CIRROS_X86_64_TAG=0.3.4
+
+wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-disk.img
+wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-lxc.tar.gz -P ${FUNCTEST_BASE_DIR}/data/
+wget http://205.177.226.237:9999/onosfw/firewall_block_image.img -P ${FUNCTEST_BASE_DIR}/data/
+
+# Add the 3-part image for aarch64, since functest can be run from an x86 machine to test an aarch64 POD
+wget ${CIRROS_REPO_URL}/daily/${CIRROS_AARCH64_TAG}/cirros-$(basename ${CIRROS_AARCH64_TAG})-aarch64-disk.img -P ${FUNCTEST_BASE_DIR}/data/
+wget ${CIRROS_REPO_URL}/daily/${CIRROS_AARCH64_TAG}/cirros-$(basename ${CIRROS_AARCH64_TAG})-aarch64-initramfs -P ${FUNCTEST_BASE_DIR}/data/
+wget ${CIRROS_REPO_URL}/daily/${CIRROS_AARCH64_TAG}/cirros-$(basename ${CIRROS_AARCH64_TAG})-aarch64-kernel -P ${FUNCTEST_BASE_DIR}/data/
