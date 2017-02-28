9815bea4fc18de05d0e9c5700ffe046ae1ad13fd
Linda Wang <wangwulin@huawei.com>
2017-02-28 11:10:12 +0000

    Bugfix: fix the cirros path for aarch64
    Docker build failed due to the wrong path for downloading cirros images for
    aarch64. The right path should be:
    http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-disk.img
    http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-initramfs
    http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-kernel
    
    Change-Id: I3f1391995c10be54e178943919dabdcf5763edde
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- docker/add_images.sh
+++ docker/add_images.sh
@@ -4,14 +4,14 @@
 # and places them in the functest docker image
 
 CIRROS_REPO_URL=http://download.cirros-cloud.net
-CIRROS_AARCH64_TAG=daily/20161201
+CIRROS_AARCH64_TAG=161201
 CIRROS_X86_64_TAG=0.3.5
 
-wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-disk.img
+wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-disk.img -P ${FUNCTEST_BASE_DIR}/data/
 wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-lxc.tar.gz -P ${FUNCTEST_BASE_DIR}/data/
 wget http://205.177.226.237:9999/onosfw/firewall_block_image.img -P ${FUNCTEST_BASE_DIR}/data/
 
 # Add the 3-part image for aarch64, since functest can be run from an x86 machine to test an aarch64 POD
-wget ${CIRROS_REPO_URL}/daily/${CIRROS_AARCH64_TAG}/cirros-$(basename ${CIRROS_AARCH64_TAG})-aarch64-disk.img -P ${FUNCTEST_BASE_DIR}/data/
-wget ${CIRROS_REPO_URL}/daily/${CIRROS_AARCH64_TAG}/cirros-$(basename ${CIRROS_AARCH64_TAG})-aarch64-initramfs -P ${FUNCTEST_BASE_DIR}/data/
-wget ${CIRROS_REPO_URL}/daily/${CIRROS_AARCH64_TAG}/cirros-$(basename ${CIRROS_AARCH64_TAG})-aarch64-kernel -P ${FUNCTEST_BASE_DIR}/data/
+wget ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-disk.img -P ${FUNCTEST_BASE_DIR}/data/
+wget ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-initramfs -P ${FUNCTEST_BASE_DIR}/data/
+wget ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-kernel -P ${FUNCTEST_BASE_DIR}/data/
