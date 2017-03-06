3e1b520214f31d1b61ba2589f6faf6f85f8edca7
spisarski <s.pisarski@cablelabs.com>
2017-03-06 12:58:34 -0700

    Added support for scenario-based flavor creation.
    As certain scenarios such as, DPDK, FDIO, and OVS use cases
    require special 'hw:' settings to flavors, SNAPS-OO has just
    implemented the ability to take in these configuration values
    to apply to its integration tests. This change checks the
    scenario and should be configuring the flavor metadata accordingly.
    
    Also see SNAPS-38
    
    Used this as an opportunity to refactor the SNAPS test classes as well...
    
    JIRA: FUNCTEST-746
    
    Change-Id: I63535776acde088a7461f602966a324b6207592d
    Signed-off-by: spisarski <s.pisarski@cablelabs.com>

--- functest/opnfv_tests/openstack/snaps/health_check.py
+++ functest/opnfv_tests/openstack/snaps/health_check.py
@@ -7,15 +7,15 @@
 
 import unittest
 
+from functest.opnfv_tests.openstack.snaps.snaps_test_runner import \
+    SnapsTestRunner
+from functest.utils.constants import CONST
+
 from snaps.openstack.tests.os_source_file_test import OSIntegrationTestCase
 from snaps.openstack.tests.create_instance_tests import SimpleHealthCheck
 
-from functest.core.pytest_suite_runner import PyTestSuiteRunner
-from functest.opnfv_tests.openstack.snaps import snaps_utils
-from functest.utils.constants import CONST
 
-
-class HealthCheck(PyTestSuiteRunner):
+class HealthCheck(SnapsTestRunner):
     """
     This test executes the SNAPS Python Test case SimpleHealthCheck which
     creates a VM with a single port with an IPv4 address that is assigned by
@@ -26,7 +26,6 @@ class HealthCheck(PyTestSuiteRunner):
 
         self.suite = unittest.TestSuite()
         self.case_name = "snaps_health_check"
-        ext_net_name = snaps_utils.get_ext_net_name()
 
         image_custom_config = None
         if hasattr(CONST, 'snaps_health_check'):
@@ -34,6 +33,7 @@ class HealthCheck(PyTestSuiteRunner):
 
         self.suite.addTest(
             OSIntegrationTestCase.parameterize(
-                SimpleHealthCheck, CONST.openstack_creds, ext_net_name,
+                SimpleHealthCheck, CONST.openstack_creds, self.ext_net_name,
                 use_keystone=CONST.snaps_use_keystone,
+                flavor_metadata=self.flavor_metadata,
                 image_metadata=image_custom_config))
--- functest/opnfv_tests/openstack/snaps/smoke.py
+++ functest/opnfv_tests/openstack/snaps/smoke.py
@@ -10,12 +10,12 @@ import unittest
 
 from snaps import test_suite_builder
 
-from functest.core.pytest_suite_runner import PyTestSuiteRunner
-from functest.opnfv_tests.openstack.snaps import snaps_utils
+from functest.opnfv_tests.openstack.snaps.snaps_test_runner import \
+    SnapsTestRunner
 from functest.utils.constants import CONST
 
 
-class SnapsSmoke(PyTestSuiteRunner):
+class SnapsSmoke(SnapsTestRunner):
     """
     This test executes the Python Tests included with the SNAPS libraries
     that exercise many of the OpenStack APIs within Keystone, Glance, Neutron,
@@ -27,7 +27,6 @@ class SnapsSmoke(PyTestSuiteRunner):
         self.suite = unittest.TestSuite()
         self.case_name = "snaps_smoke"
         use_fip = CONST.snaps_use_floating_ips
-        ext_net_name = snaps_utils.get_ext_net_name()
 
         # Tests requiring floating IPs leverage files contained within the
         # SNAPS repository and are found relative to that path
@@ -38,6 +37,6 @@ class SnapsSmoke(PyTestSuiteRunner):
         test_suite_builder.add_openstack_integration_tests(
             self.suite,
             CONST.openstack_creds,
-            ext_net_name,
+            self.ext_net_name,
             use_keystone=CONST.snaps_use_keystone,
             use_floating_ips=use_fip)
