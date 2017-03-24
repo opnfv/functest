3e291782a25b9b41352c0ce2ff38c69c4debed7f
randyl <r.levensalor@cablelabs.com>
2017-03-24 08:51:35 -0600

    Printing the flavor_metadata for SnapsTestRunner
    Need to print what is being tested to debug and
    verify results.
    
    JIRA : FUNCTEST-768
    
    This is needed by the Apex team for the danube release.
    
    Change-Id: Ie7400af14f7e54d1deb5b2b5d2fc91eb5b14ca91
    Signed-off-by: randyl <r.levensalor@cablelabs.com>

--- functest/opnfv_tests/openstack/snaps/snaps_test_runner.py
+++ functest/opnfv_tests/openstack/snaps/snaps_test_runner.py
@@ -5,6 +5,7 @@
 #
 # http://www.apache.org/licenses/LICENSE-2.0
 
+from functest.utils import functest_logger as ft_logger
 from functest.core.pytest_suite_runner import PyTestSuiteRunner
 from functest.opnfv_tests.openstack.snaps import snaps_utils
 from functest.utils import functest_utils
@@ -22,9 +23,11 @@ class SnapsTestRunner(PyTestSuiteRunner):
         super(SnapsTestRunner, self).__init__()
 
         self.ext_net_name = snaps_utils.get_ext_net_name()
-
+        self.logger = ft_logger.Logger(self.project_name).getLogger()
         scenario = functest_utils.get_scenario()
 
         self.flavor_metadata = create_flavor.MEM_PAGE_SIZE_ANY
         if 'ovs' in scenario or 'fdio' in scenario:
             self.flavor_metadata = create_flavor.MEM_PAGE_SIZE_LARGE
+
+        self.logger.info("Using flavor metatdata '%s'" % self.flavor_metadata)
