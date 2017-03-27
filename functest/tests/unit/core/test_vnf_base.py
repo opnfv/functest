d668bc68e138b349396da7ff87288339692393b0
ashishk1994 <ashishk.iiit@gmail.com>
2017-03-27 12:26:20 +0530

    More Unit Tests with coverage > 80%
    This patch adds more unit tests for core/vnf_base,
    openstack/tempest and ims/ modules
    
    Change-Id: I7c18cea791ac8dc3e890da98066179225283b068
    Signed-off-by: ashishk1994 <ashishk.iiit@gmail.com>

--- functest/tests/unit/core/test_vnf_base.py
+++ functest/tests/unit/core/test_vnf_base.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2016 Orange and others.
-#
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Apache License, Version 2.0
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 
 import logging
+import os
 import unittest
+
+import mock
 
 from functest.core import vnf_base
 
@@ -34,6 +35,142 @@ class VnfBaseTesting(unittest.TestCase):
                              "test_vnf": {"status": "FAIL",
                                           "result": "",
                                           "duration": 5}}
+        self.test.keystone_client = 'test_client'
+        self.test.tenant_name = 'test_tenant_name'
+
+    def test_execute_deploy_vnf_fail(self):
+        with mock.patch.object(self.test, 'prepare'),\
+            mock.patch.object(self.test, 'deploy_orchestrator',
+                              return_value=None), \
+            mock.patch.object(self.test, 'deploy_vnf',
+                              side_effect=Exception), \
+                self.assertRaises(Exception):
+            self.test.execute()
+
+    def test_execute_test_vnf_fail(self):
+        with mock.patch.object(self.test, 'prepare'),\
+            mock.patch.object(self.test, 'deploy_orchestrator',
+                              return_value=None), \
+            mock.patch.object(self.test, 'deploy_vnf'), \
+            mock.patch.object(self.test, 'test_vnf',
+                              side_effect=Exception), \
+                self.assertRaises(Exception):
+            self.test.execute()
+
+    def test_execute_default(self):
+        with mock.patch.object(self.test, 'prepare'),\
+            mock.patch.object(self.test, 'deploy_orchestrator',
+                              return_value=None), \
+            mock.patch.object(self.test, 'deploy_vnf'), \
+            mock.patch.object(self.test, 'test_vnf'), \
+            mock.patch.object(self.test, 'parse_results',
+                              return_value='ret_exit_code'), \
+            mock.patch.object(self.test, 'log_results'), \
+            mock.patch('functest.core.vnf_base.os_utils.get_tenant_id',
+                       return_value='test_tenant_id'), \
+            mock.patch('functest.core.vnf_base.os_utils.delete_tenant',
+                       return_value=True), \
+            mock.patch('functest.core.vnf_base.os_utils.get_user_id',
+                       return_value='test_user_id'), \
+            mock.patch('functest.core.vnf_base.os_utils.delete_user',
+                       return_value=True):
+            self.assertEqual(self.test.execute(),
+                             'ret_exit_code')
+
+    def test_prepare_missing_userid(self):
+        with mock.patch('functest.core.vnf_base.'
+                        'os_utils.get_credentials'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_keystone_client'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_user_id',
+                       return_value=''), \
+                self.assertRaises(Exception):
+            self.test.prepare()
+
+    def test_prepare_missing_tenantid(self):
+        with mock.patch('functest.core.vnf_base.'
+                        'os_utils.get_credentials'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_keystone_client'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_user_id',
+                       return_value='test_roleid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.create_tenant',
+                       return_value=''), \
+                self.assertRaises(Exception):
+            self.test.prepare()
+
+    def test_prepare_missing_roleid(self):
+        with mock.patch('functest.core.vnf_base.'
+                        'os_utils.get_credentials'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_keystone_client'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_user_id',
+                       return_value='test_roleid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.create_tenant',
+                       return_value='test_tenantid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_role_id',
+                       return_value=''), \
+                self.assertRaises(Exception):
+            self.test.prepare()
+
+    def test_prepare_role_add_failure(self):
+        with mock.patch('functest.core.vnf_base.'
+                        'os_utils.get_credentials'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_keystone_client'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_user_id',
+                       return_value='test_roleid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.create_tenant',
+                       return_value='test_tenantid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_role_id',
+                       return_value='test_roleid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.add_role_user',
+                       return_value=''), \
+                self.assertRaises(Exception):
+            self.test.prepare()
+
+    def test_prepare_create_user_failure(self):
+        with mock.patch('functest.core.vnf_base.'
+                        'os_utils.get_credentials'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_keystone_client'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_user_id',
+                       return_value='test_roleid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.create_tenant',
+                       return_value='test_tenantid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.get_role_id',
+                       return_value='test_roleid'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.add_role_user'), \
+            mock.patch('functest.core.vnf_base.'
+                       'os_utils.create_user',
+                       return_value=''), \
+                self.assertRaises(Exception):
+            self.test.prepare()
+
+    def test_log_results_default(self):
+        with mock.patch('functest.core.vnf_base.'
+                        'ft_utils.logger_test_results') \
+                as m:
+            self.test.log_results()
+            self.assertTrue(m.called)
+
+    def test_step_failures_default(self):
+        with self.assertRaises(Exception):
+            self.test.step_failure()
 
     def test_deploy_vnf_unimplemented(self):
         with self.assertRaises(Exception) as context:
@@ -45,8 +182,13 @@ class VnfBaseTesting(unittest.TestCase):
             self.test.test_vnf()()
         self.assertTrue('VNF not tested' in context.exception)
 
-    def test_parse_results(self):
-        self.assertNotEqual(self.test.parse_results(), 0)
+    def test_parse_results_ex_ok(self):
+        self.test.details['test_vnf']['status'] = 'PASS'
+        self.assertEqual(self.test.parse_results(), os.EX_OK)
+
+    def test_parse_results_ex_run_error(self):
+        self.test.details['vnf']['status'] = 'FAIL'
+        self.assertEqual(self.test.parse_results(), os.EX_SOFTWARE)
 
 
 if __name__ == "__main__":
