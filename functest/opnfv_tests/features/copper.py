27bce273c3201cac205256df375ec66152505056
Aimee Ukasick <aimeeu.opensource@gmail.com>
2017-03-27 14:02:42 -0500

    Uncomment Copper section in testcases.yaml
    JIRA: COPPER-34
    
    Tested on Apex 4.0-20170319.noarch.rpm installation on Intel
    POD 19.
    
    ======================
    Test Execution Summary
    ======================
    Test Duration = 6 minutes
    Passed : dmz
    Passed : smtp_ingress
    Passed : reserved_subnet
    Test run overall: PASSED
    /home/opnfv
    
    Change-Id: I7301745718b9c035493c2e5c162e2a4e117982db
    Signed-off-by: Aimee Ukasick <aimeeu.opensource@gmail.com>

--- functest/opnfv_tests/features/copper.py
+++ functest/opnfv_tests/features/copper.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright 2016 AT&T Intellectual Property, Inc
+# Copyright 2017 AT&T Intellectual Property, Inc
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
