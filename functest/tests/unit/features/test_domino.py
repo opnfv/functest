07f87b34af62650f54f385f36a57f033d3a0126d
Cédric Ollivier <cedric.ollivier@orange.com>
2017-04-01 14:40:10 +0200

    Add unit tests for domino
    Change-Id: Ie6671080a3d38a17da0ee608a362605a6d9df9db
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ functest/tests/unit/features/test_domino.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2017 Orange and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# pylint: disable=missing-docstring
+
+import logging
+import unittest
+
+from functest.opnfv_tests.features import domino
+from functest.utils import constants
+
+
+class DominoTesting(unittest.TestCase):
+
+    logging.disable(logging.CRITICAL)
+
+    def setUp(self):
+        self.domino = domino.Domino()
+
+    def test_init(self):
+        self.assertEqual(self.domino.project_name, "domino")
+        self.assertEqual(self.domino.case_name, "domino-multinode")
+        self.assertEqual(
+            self.domino.repo,
+            constants.CONST.__getattribute__("dir_repo_domino"))
+        self.assertEqual(
+            self.domino.cmd,
+            'cd {} && ./tests/run_multinode.sh'.format(self.domino.repo))
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
