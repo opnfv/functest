70b1313fe9528a72fab966ab69e7d784e40653d5
Cédric Ollivier <cedric.ollivier@orange.com>
2017-04-01 14:46:31 +0200

    Add unit tests for netready
    Change-Id: I45f9209c55bd65c9538fc3b1181ccbcfbdd23a40
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ functest/tests/unit/features/test_netready.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2017 Orange and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# pylint: disable=missing-docstring
+
+import logging
+import unittest
+
+from functest.opnfv_tests.features import netready
+from functest.utils import constants
+
+
+class NetreadyTesting(unittest.TestCase):
+
+    logging.disable(logging.CRITICAL)
+
+    def setUp(self):
+        self.netready = netready.GluonVping()
+
+    def test_init(self):
+        self.assertEqual(self.netready.project_name, "netready")
+        self.assertEqual(self.netready.case_name, "gluon_vping")
+        self.assertEqual(
+            self.netready.repo,
+            constants.CONST.__getattribute__("dir_repo_netready"))
+        self.assertEqual(
+            self.netready.cmd,
+            'cd {}/test/functest && python ./gluon-test-suite.py'.format(
+                self.netready.repo))
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
