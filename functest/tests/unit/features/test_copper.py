107f7045ba25eed1a5927c7a2f17a260a04bb231
Cédric Ollivier <cedric.ollivier@orange.com>
2017-04-01 14:12:30 +0200

    Add unit tests for copper
    Change-Id: Ia4e53e2aee5b93071b3acd3d75c7e42841321a0a
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ functest/tests/unit/features/test_copper.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2017 Orange and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# pylint: disable=missing-docstring
+
+import logging
+import unittest
+
+from functest.opnfv_tests.features import copper
+from functest.utils import constants
+
+
+class CopperTesting(unittest.TestCase):
+
+    logging.disable(logging.CRITICAL)
+
+    def setUp(self):
+        self.copper = copper.Copper()
+
+    def test_init(self):
+        self.assertEqual(self.copper.project_name, "copper")
+        self.assertEqual(self.copper.case_name, "copper-notification")
+        self.assertEqual(
+            self.copper.repo,
+            constants.CONST.__getattribute__("dir_repo_copper"))
+        self.assertEqual(
+            self.copper.cmd,
+            "cd /home/opnfv/repos/copper/tests && bash run.sh && cd -")
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
