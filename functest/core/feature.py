3c25d150547f68c8d8b8772fba3ac3da1409b9b0
SerenaFeng <feng.xiaowei@zte.com.cn>
2017-04-14 14:45:36 +0800

    add license in feature.py
    add apache license in feature.py file
    
    Change-Id: I75f51f216dcbf0db1c563cf381c28efc47241d3c
    Signed-off-by: SerenaFeng <feng.xiaowei@zte.com.cn>

--- functest/core/feature.py
+++ functest/core/feature.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2016 ZTE Corp and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+"""Define the parent class of Feature TestCase."""
+
 import time
 
 import functest.core.testcase as base
