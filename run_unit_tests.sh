28f484fdd6da98157567345ca6ca9ee41b265ff5
ashishk1994 <ashishk.iiit@gmail.com>
2017-04-22 22:10:31 +0530

    Tox utility to run unit tests
    Change-Id: I934f4ac739d99d0cb29bea8c6aac6d6780b30901
    Signed-off-by: ashishk1994 <ashishk.iiit@gmail.com>

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -2,28 +2,6 @@
 set -o errexit
 set -o pipefail
 
-# Either Workspace is set (CI)
-if [ -z $WORKSPACE ]
-then
-    WORKSPACE=`pwd`
-fi
-
-
-# ***************
-# Run unit tests
-# ***************
-echo "Running unit tests..."
-
-# start vitual env
-virtualenv $WORKSPACE/functest_venv
-source $WORKSPACE/functest_venv/bin/activate
-
-# install python packages
-sudo apt-get install -y build-essential python-dev python-pip
-pip install --upgrade pip
-pip install -r $WORKSPACE/test-requirements.txt
-pip install $WORKSPACE
-
 #install releng
 rm -rf releng-unittests
 git clone --depth 1 https://gerrit.opnfv.org/gerrit/releng releng-unittests
@@ -51,7 +29,5 @@ nosetests --with-xunit \
          --log-config=$(pwd)/functest/tests/unit/test_logging.ini \
          functest/tests/unit
 rc=$?
-
-deactivate
 
 exit $rc
