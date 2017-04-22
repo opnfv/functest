3e7d198d279347c219b0295ce058bcbd7b550f13
ashishk1994 <ashishk.iiit@gmail.com>
2017-04-22 22:10:31 +0530

    Tox utility to run unit tests
    Co-Authored-By: Cédric Ollivier <cedric.ollivier@orange.com>
    
    Change-Id: I934f4ac739d99d0cb29bea8c6aac6d6780b30901
    Signed-off-by: ashishk1994 <ashishk.iiit@gmail.com>
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -2,31 +2,6 @@
 set -o errexit
 set -o pipefail
 
-# Either Workspace is set (CI)
-if [ -z $WORKSPACE ]
-then
-    WORKSPACE=`pwd`
-fi
-
-
-# ***************
-# Run unit tests
-# ***************
-echo "Running unit tests..."
-
-sudo apt-get install -y build-essential python-dev python-pip
-sudo pip install virtualenv==15.1.0
-
-# start vitual env
-virtualenv $WORKSPACE/functest_venv
-source $WORKSPACE/functest_venv/bin/activate
-
-# install python packages
-pip install --upgrade pip
-pip install -r $WORKSPACE/requirements.txt
-pip install -r $WORKSPACE/test-requirements.txt
-pip install $WORKSPACE
-
 #install releng
 rm -rf releng-unittests
 git clone --depth 1 https://gerrit.opnfv.org/gerrit/releng releng-unittests
@@ -36,7 +11,7 @@ rm -rf releng-unittests
 #install barometer
 rm -rf barometer-unittests
 git clone --depth 1 https://gerrit.opnfv.org/gerrit/barometer barometer-unittests
-(cd barometer-unittests; python setup.py install)
+(cd barometer-unittests; pip install .)
 rm -rf barometer-unittests
 
 nosetests --with-xunit \
@@ -47,8 +22,5 @@ nosetests --with-xunit \
          --cover-html \
          --log-config=$(pwd)/functest/tests/unit/test_logging.ini \
          functest/tests/unit
-rc=$?
 
-deactivate
-
-exit $rc
+exit $?
