81d465e83a4e189e1694b8cc466a58a849809be7
helenyao <yaohelan@huawei.com>
2017-04-26 17:10:47 +0800

    Fix the instance initialization error for OnosSfc
    Change-Id: I1820bed1bbb7dbddd3923ea6439f58592595f5c8
    Signed-off-by: helenyao <yaohelan@huawei.com>

--- functest/opnfv_tests/sdn/onos/onos.py
+++ functest/opnfv_tests/sdn/onos/onos.py
@@ -25,12 +25,16 @@ logger = ft_logger.Logger(__name__).getLogger()
 
 
 class OnosBase(testcase.TestCase):
-    onos_repo_path = CONST.dir_repo_onos
-    onos_sfc_image_name = CONST.onos_sfc_image_name
-    onos_sfc_image_path = os.path.join(CONST.dir_functest_data,
-                                       CONST.onos_sfc_image_file_name)
-    onos_sfc_path = os.path.join(CONST.dir_repo_functest,
-                                 CONST.dir_onos_sfc)
+    def __init__(self, **kwargs):
+        if "case_name" not in kwargs:
+            kwargs["case_name"] = "onos_base"
+        super(OnosBase, self).__init__(**kwargs)
+        self.onos_repo_path = CONST.dir_repo_onos
+        self.onos_sfc_image_name = CONST.onos_sfc_image_name
+        self.onos_sfc_image_path = os.path.join(CONST.dir_functest_data,
+                                                CONST.onos_sfc_image_file_name)
+        self.onos_sfc_path = os.path.join(CONST.dir_repo_functest,
+                                          CONST.dir_onos_sfc)
 
     def run(self):
         self.start_time = time.time()
@@ -155,7 +159,7 @@ class Onos(OnosBase):
             if (result['FUNCvirNet']['result'] == "Success" and
                     result['FUNCvirNetL3']['result'] == "Success"):
                 status = "PASS"
-        except:
+        except Exception:
             logger.error("Unable to set ONOS criteria")
 
         self.criteria = status
@@ -170,9 +174,10 @@ class Onos(OnosBase):
 
 
 class OnosSfc(OnosBase):
-    def __init__(self):
-        super(OnosSfc, self).__init__()
-        self.case_name = 'onos_sfc'
+    def __init__(self, **kwargs):
+        if "case_name" not in kwargs:
+            kwargs["case_name"] = "onos_sfc"
+        super(OnosSfc, self).__init__(**kwargs)
 
     def get_ip(type):
         url = openstack_utils.get_endpoint(service_type=type)
