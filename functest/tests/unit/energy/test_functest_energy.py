5c99fee563dd28d04fa9e64ae13d5c3e96e38abe
Benoit HERARD <benoit.herard@orange.com>
2017-04-27 11:23:00 +0200

    Add Energy recording support
    It adds helpers to send notifications to Energy recording API and
    related unit tests.
    It requires a dedicated section in functest config file
    to set connectivity parameters to Energy recording API.
    
    Change-Id: Idcb74d1bf7341ccce7cc1c3926f22338ce24f714
    Signed-off-by: Benoit HERARD <benoit.herard@orange.com>

---
+++ functest/tests/unit/energy/test_functest_energy.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+"""Unitary test for energy module."""
+
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import logging
+import unittest
+
+import mock
+
+from functest.energy.energy import EnergyRecorder
+import functest.energy.energy as energy
+
+
+CASE_NAME = "UNIT_TEST_CASE"
+STEP_NAME = "UNIT_TEST_STEP"
+
+logging.disable(logging.CRITICAL)
+
+
+class MockHttpResponse(object):  # pylint: disable=too-few-public-methods
+    """Mock response for Energy recorder API."""
+
+    def __init__(self, text, status_code):
+        """Create an instance of MockHttpResponse."""
+        self.text = text
+        self.status_code = status_code
+
+
+RECORDER_OK = MockHttpResponse(
+    '{"environment": "UNIT_TEST",'
+    ' "step": "string",'
+    ' "scenario": "' + CASE_NAME + '"}',
+    200
+)
+RECORDER_KO = MockHttpResponse(
+    '{"message": "An unhandled API exception occurred (MOCK)"}',
+    500
+)
+
+
+class EnergyRecorderTest(unittest.TestCase):
+    """Energy module unitary test suite."""
+
+    case_name = CASE_NAME
+
+    @mock.patch('functest.energy.energy.requests.post',
+                return_value=RECORDER_OK)
+    def testtest_start(self, post_mock=None):
+        """EnergyRecorder.start method (regular case)."""
+        self.assertTrue(EnergyRecorder.start(self.case_name))
+
+    @mock.patch('functest.energy.energy.requests.post',
+                side_effect=Exception("Internal execution error (MOCK)"))
+    def testtest_start_error(self, post_mock=None):
+        """EnergyRecorder.start method (error in method)."""
+        self.assertFalse(EnergyRecorder.start(self.case_name))
+
+    @mock.patch('functest.energy.energy.requests.post',
+                return_value=RECORDER_KO)
+    def testtest_start_api_error(self, post_mock=None):
+        """EnergyRecorder.start method (API error)."""
+        self.assertFalse(EnergyRecorder.start(self.case_name))
+
+    @mock.patch('functest.energy.energy.requests.post',
+                return_value=RECORDER_OK)
+    def testtest_set_step(self, post_mock=None):
+        """EnergyRecorder.set_step method (regular case)."""
+        self.assertTrue(EnergyRecorder.set_step(STEP_NAME))
+
+    @mock.patch('functest.energy.energy.requests.post',
+                return_value=RECORDER_KO)
+    def testtest_set_step_api_error(self, post_mock=None):
+        """EnergyRecorder.set_step method (API error)."""
+        self.assertFalse(EnergyRecorder.set_step(STEP_NAME))
+
+    @mock.patch('functest.energy.energy.requests.post',
+                side_effect=Exception("Internal execution error (MOCK)"))
+    def testtest_set_step_error(self, post_mock=None):
+        """EnergyRecorder.set_step method (method error)."""
+        self.assertFalse(EnergyRecorder.set_step(STEP_NAME))
+
+    @mock.patch('functest.energy.energy.requests.delete',
+                return_value=RECORDER_OK)
+    def testtest_stop(self, delete_mock=None):
+        """EnergyRecorder.stop method (regular case)."""
+        self.assertTrue(EnergyRecorder.stop())
+
+    @mock.patch('functest.energy.energy.requests.delete',
+                return_value=RECORDER_KO)
+    def testtest_stop_api_error(self, delete_mock=None):
+        """EnergyRecorder.stop method (API Error)."""
+        self.assertFalse(EnergyRecorder.stop())
+
+    @mock.patch('functest.energy.energy.requests.delete',
+                side_effect=Exception("Internal execution error (MOCK)"))
+    def testtest_stop_error(self, delete_mock=None):
+        """EnergyRecorder.stop method (method error)."""
+        self.assertFalse(EnergyRecorder.stop())
+
+    @energy.set_step(STEP_NAME)
+    def do_some_stuff(self):
+        """Fake method to test energy.test_step decorator."""
+        pass
+
+    @energy.enable_recording
+    def decorated_method(self):
+        """Call with to energy recorder decorators."""
+        self.do_some_stuff()
+
+    @mock.patch('functest.energy.energy.EnergyRecorder')
+    def test_decorators(self, recorder_mock):
+        """Test energy module decorators."""
+        self.decorated_method()
+        calls = [mock.call.start(self.case_name),
+                 mock.call.set_step(STEP_NAME),
+                 mock.call.stop()]
+        recorder_mock.assert_has_calls(calls)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
