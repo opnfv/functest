57c9a144cb547c8bbbb94fe847b699ecd5ae3b98
Cédric Ollivier <cedric.ollivier@orange.com>
2017-04-30 09:38:55 +0200

    Restrict coverage output to all functest packages
    The next packages/modules are not yet covered:
      - functest/cli/setup.py
      - functest/core/pytest_suite_runner.py
      - functest/opnfv_tests/mano
      - functest/opnfv_tests/openstack/examples
      - functest/opnfv_tests/openstack/snaps
      - functest/opnfv_tests/openstack/vping
      - functest/opnfv_tests/sdn/onos
      - functest/opnfv_tests/vnf/aaa
      - functest/opnfv_tests/vnf/ims
      - functest/opnfv_tests/vnf/router
      - functest/utils/openstack
    
    Change-Id: Id62749796864537615f0ca8b162ce77b6c1a3309
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -31,21 +31,10 @@ pip install releng-unittests/modules/
 rm -fr releng-unittests
 
 export CONFIG_FUNCTEST_YAML=$(pwd)/functest/ci/config_functest.yaml
-# unit tests
-# TODO: remove cover-erase
-# To be deleted when all functest packages will be listed
 nosetests --with-xunit \
          --with-coverage \
-         --cover-erase \
          --cover-tests \
-         --cover-package=functest.ci \
-         --cover-package=functest.cli \
-         --cover-package=functest.core \
-         --cover-package=functest.opnfv_tests.sdn.odl.odl \
-         --cover-package=functest.opnfv_tests.vnf.ims \
-         --cover-package=functest.utils \
-         --cover-package=functest.opnfv_tests.features \
-         --cover-package=functest.opnfv_tests.openstack \
+         --cover-package=functest \
          --cover-xml \
          --cover-html \
          --log-config=$(pwd)/functest/tests/unit/test_logging.ini \
