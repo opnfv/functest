73e02c09a67bcd2ca54d82ced31372b86ec914f5
helenyao <yaohelan@huawei.com>
2017-05-02 14:24:22 +0800

    Pack image for bgpvpn testcase to docker container
    In order to execute Functest offline, required images need to be packed
    in docker image in advance to avoid downloading during runtime.
    
    Change-Id: I0d309b93f52b3da23d6130056f1a19907313ef68
    Signed-off-by: helenyao <yaohelan@huawei.com>

--- docker/add_images.sh
+++ docker/add_images.sh
@@ -10,6 +10,7 @@ CIRROS_X86_64_TAG=0.3.5
 wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-disk.img -P ${FUNCTEST_BASE_DIR}/data/
 wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-lxc.tar.gz -P ${FUNCTEST_BASE_DIR}/data/
 wget http://205.177.226.237:9999/onosfw/firewall_block_image.img -P ${FUNCTEST_BASE_DIR}/data/
+wget http://artifacts.opnfv.org/sdnvpn/ubuntu-16.04-server-cloudimg-amd64-disk1.img -P ${FUNCTEST_BASE_DIR}/data/
 
 # Add the 3-part image for aarch64, since functest can be run from an x86 machine to test an aarch64 POD
 wget ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-disk.img -P ${FUNCTEST_BASE_DIR}/data/
