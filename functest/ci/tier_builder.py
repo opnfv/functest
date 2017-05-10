f444341fb26dbd9f7c33b923edd10e44608678f8
Cédric Ollivier <cedric.ollivier@orange.com>
2017-05-10 11:38:32 +0200

    Fix how to get tier_name
    The tier name was incorrect when passing a test case as run_test.py arg.
    
    Change-Id: I6f439fc7b90facb3f0553798bd1fe06ac9f09475
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/ci/tier_builder.py
+++ functest/ci/tier_builder.py
@@ -72,6 +72,12 @@ class TierBuilder(object):
                 return self.tier_objects[i]
         return None
 
+    def get_tier_name(self, test_name):
+        for i in range(0, len(self.tier_objects)):
+            if self.tier_objects[i].is_test(test_name):
+                return self.tier_objects[i].name
+        return None
+
     def get_test(self, test_name):
         for i in range(0, len(self.tier_objects)):
             if self.tier_objects[i].is_test(test_name):
