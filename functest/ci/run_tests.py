7420518318aa3216ff17fbde3d9180b3bd72f78d
Cédric Ollivier <cedric.ollivier@orange.com>
2017-05-28 12:51:22 +0200

    Switch shebangs to /usr/bin/env python
    It modified the shebang to /usr/bin/env which is considered as more
    flexible.
    
    Change-Id: I9c8cdb4243d41a83d524499cc91060d3700e008b
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/ci/run_tests.py
+++ functest/ci/run_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/env python
 #
 # Author: Jose Lausuch (jose.lausuch@ericsson.com)
 #
--- functest/opnfv_tests/openstack/tempest/tempest.py
+++ functest/opnfv_tests/openstack/tempest/tempest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #
 # Copyright (c) 2015 All rights reserved
 # This program and the accompanying materials
