11c8eea26450cbaad8ff0a5f78ae19e53d463ecf
spisarski <s.pisarski@cablelabs.com>
2017-05-31 12:46:36 -0600

    SNAPS tests now support offline testing.
    Images by type (cirros, centos, ubuntu) can now have the download URL be
    overridden, told to leverage a local file, or have custom configuration
    when using the snaps.openstack.tests.openstack_tests.py image settings
    helper functions.
    
    JIRA: FUNCTEST-809
    
    Change-Id: I790094a7a26eb5a55f53d6e875c658dc5e7ac2f9
    Signed-off-by: spisarski <s.pisarski@cablelabs.com>

--- functest/opnfv_tests/openstack/snaps/health_check.py
+++ functest/opnfv_tests/openstack/snaps/health_check.py
@@ -9,7 +9,6 @@ import unittest
 
 from functest.opnfv_tests.openstack.snaps.snaps_test_runner import \
     SnapsTestRunner
-from functest.utils.constants import CONST
 
 from snaps.openstack.tests.os_source_file_test import OSIntegrationTestCase
 from snaps.openstack.tests.create_instance_tests import SimpleHealthCheck
@@ -27,15 +26,10 @@ class HealthCheck(SnapsTestRunner):
         super(HealthCheck, self).__init__(**kwargs)
 
         self.suite = unittest.TestSuite()
-
-        image_custom_config = None
-
-        if hasattr(CONST, 'snaps_images_cirros'):
-            image_custom_config = CONST.__getattribute__('snaps_images_cirros')
         self.suite.addTest(
             OSIntegrationTestCase.parameterize(
                 SimpleHealthCheck, os_creds=self.os_creds,
                 ext_net_name=self.ext_net_name,
                 use_keystone=self.use_keystone,
                 flavor_metadata=self.flavor_metadata,
-                image_metadata=image_custom_config))
+                image_metadata=self.image_metadata))
--- functest/opnfv_tests/openstack/snaps/smoke.py
+++ functest/opnfv_tests/openstack/snaps/smoke.py
@@ -28,13 +28,11 @@ class SnapsSmoke(SnapsTestRunner):
 
         self.suite = unittest.TestSuite()
 
-        image_config = None
-        if hasattr(CONST, 'snaps_images_cirros'):
-            image_config = CONST.__getattribute__('snaps_images_cirros')
-
         # Tests requiring floating IPs leverage files contained within the
         # SNAPS repository and are found relative to that path
         if self.use_fip:
+            # 3 tests use a relative path for obtaining Ansible Playbooks that
+            # are applied via a floating IP
             snaps_dir = os.path.join(CONST.__getattribute__('dir_repo_snaps'),
                                      'snaps')
             os.chdir(snaps_dir)
@@ -45,5 +43,5 @@ class SnapsSmoke(SnapsTestRunner):
             ext_net_name=self.ext_net_name,
             use_keystone=self.use_keystone,
             flavor_metadata=self.flavor_metadata,
-            image_metadata=image_config,
+            image_metadata=self.image_metadata,
             use_floating_ips=self.use_fip)
