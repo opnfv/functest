3c5697ca01dcf66e9c955d5d625fc1dfe6945a45
Linda Wang <wangwulin@huawei.com>
2017-06-02 10:26:15 +0000

    Delete duplicated constants module
    1. Delete duplicated constants module in functest_utils.
    2. The module constants should be imported rather than the object
       in test_functest_utils.
    
    Change-Id: Ia6381c836dc1bc629e6ad8dd96e93f406dc99d60
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- functest/utils/functest_utils.py
+++ functest/utils/functest_utils.py
@@ -26,7 +26,6 @@ from git import Repo
 
 from functest.utils import constants
 from functest.utils import decorators
-from functest.utils.constants import CONST
 
 logger = logging.getLogger(__name__)
 
@@ -158,7 +157,7 @@ def logger_test_results(project, case_name, status, details):
     scenario = get_scenario()
     version = get_version()
     build_tag = get_build_tag()
-    db_url = CONST.__getattribute__("results_test_db_url")
+    db_url = constants.CONST.__getattribute__("results_test_db_url")
 
     logger.info(
         "\n"
@@ -190,7 +189,7 @@ def push_results_to_db(project, case_name,
     POST results to the Result target DB
     """
     # Retrieve params from CI and conf
-    url = CONST.__getattribute__("results_test_db_url")
+    url = constants.CONST.__getattribute__("results_test_db_url")
 
     try:
         installer = os.environ['INSTALLER_TYPE']
