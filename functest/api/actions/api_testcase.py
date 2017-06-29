6c77dc94a48556ff02332f25f144e6916a7813e7
Linda Wang <wangwulin@huawei.com>
2017-06-29 07:12:29 +0000

    API proposal for functest
    1. Propose a basic framework for API
    2. And these functions have been realized:
       1) Show environment
       2) Prepare Environment
       3) Show credentials
       4) List all testcases
       5) Show a testcase
       6) List all tiers
       7) Show a tier
       8) List all testcases within given tier
    
    JIRA: FUNCTEST-843
    
    Change-Id: Ib961446708077b56465eda0052f6d38806b62594
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

---
+++ functest/api/actions/api_testcase.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2017 Huawei Technologies Co.,Ltd and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+from functest.cli.commands.cli_testcase import Testcase
+
+
+class ApiTestcase(Testcase):
+
+    def __init__(self):
+        super(ApiTestcase, self).__init__()
+
+    def list(self):
+        return super(ApiTestcase, self).list()
+
+    def show(self, testname):
+        return super(ApiTestcase, self).show(testname)
