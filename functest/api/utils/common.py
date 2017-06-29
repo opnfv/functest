651f6c1f2427188c591fb12acac4a9e0feda51a0
Linda Wang <wangwulin@huawei.com>
2017-06-29 07:12:29 +0000

    [WIP] API proposal for functest
    1. Propose a basic framework for API
    2. And these functions have been realized:
       1) Show environment
       2) List all testcases
       3) List all tiers
    
    JIRA: FUNCTEST-843
    
    Change-Id: Ib961446708077b56465eda0052f6d38806b62594
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

---
+++ functest/api/utils/common.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2017 Huawei Technologies Co.,Ltd and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+import collections
+import logging
+
+from flask import jsonify
+import six
+
+
+logger = logging.getLogger(__name__)
+
+
+def translate_to_str(obj):
+    if isinstance(obj, collections.Mapping):
+        return {str(k): translate_to_str(v) for k, v in obj.items()}
+    elif isinstance(obj, list):
+        return [translate_to_str(ele) for ele in obj]
+    elif isinstance(obj, six.text_type):
+        return str(obj)
+    return obj
+
+
+def error_handler(message):
+    logger.debug(message)
+    result = {
+        'status': 'error',
+        'message': message
+    }
+    return jsonify(result)
+
+
+class Url(object):
+
+    def __init__(self, url, resource, endpoint):
+        super(Url, self).__init__()
+        self.url = url
+        self.resource = resource
+        self.endpoint = endpoint
