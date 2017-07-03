8cb3aa971adf99146a462264b9f3e1c74dd17d64
Linda Wang <wangwulin@huawei.com>
2017-07-03 08:03:28 +0000

    Update the link to firewall_block_image.img
    As the link http://205.177.226.237:9999/onosfw/firewall_block_image.img
    has been deprecated, now the valid link is updated, aiming at running the
    testcase onos_sfc.
    
    JIRA: FUNCTEST-820
    
    Change-Id: Id848e3ab92c3b0fb47e3c755d203844dd94d8408
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- docker/add_images.sh
+++ docker/add_images.sh
@@ -2,13 +2,15 @@
 #
 # This script downloads the images that are used for testing
 # and places them in the functest docker image
+set -e
 
-CIRROS_REPO_URL=http://download.cirros-cloud.net
+CIRROS_REPO_URL=https://download.cirros-cloud.net
 CIRROS_AARCH64_TAG=161201
 CIRROS_X86_64_TAG=0.3.5
 
 wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-disk.img -P ${FUNCTEST_IMAGES_DIR}
 wget ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-lxc.tar.gz -P ${FUNCTEST_IMAGES_DIR}
+wget http://artifacts.opnfv.org/onosfw/images/firewall_block_image.img -P ${FUNCTEST_IMAGES_DIR}
 
 # Add the 3-part image for aarch64, since functest can be run from an x86 machine to test an aarch64 POD
 wget ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-disk.img -P ${FUNCTEST_IMAGES_DIR}
