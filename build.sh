e702ed692295063aa876f5cdf42b7dee00031bb7
Cédric Ollivier <cedric.ollivier@orange.com>
2017-07-11 23:53:36 +0200

    Add script to build all containers
    It also sets opnfv as repository as they will be pushed by releng.
    
    Change-Id: I08b55b1ff334afe6c0f58af3100cfa04f76d8ef0
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+(cd docker && docker build -t opnfv/functest .)
+docker push opnfv/functest-$(basename $dir)
+
+for dir in docker/core docker/healthcheck docker/smoke; do
+  (cd $dir && docker build -t opnfv/functest-$(basename $dir) .)
+  docker push opnfv/functest-$(basename $dir)
+done
+
+exit $?
