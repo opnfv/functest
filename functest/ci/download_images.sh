1cee48c654a32cad26f136481924a70138cd2059
jose.lausuch <jose.lausuch@ericsson.com>
2017-07-13 15:54:12 +0200

    Add cirros image for Parser test case
    For Functest offline to work, we need to add this image
    which is used by parser project.
    It will only download it for ha scenarios as it is required
    by Parser test case.
    
    Change-Id: I1156e99ad4727acbf71ccb5020a884cf02cffc9d
    Signed-off-by: jose.lausuch <jose.lausuch@ericsson.com>

--- functest/ci/download_images.sh
+++ functest/ci/download_images.sh
@@ -8,19 +8,19 @@ RED='\033[1;31m'
 NC='\033[0m' # No Color
 
 function usage(){
-    echo -e "${RED}USAGE: $script <destination_folder> <scenario_name> [arch]${NC}"
+    echo -e "${RED}USAGE: $script <download_dir> <scenario_name> [arch]${NC}"
     exit 0
 }
 
 script=`basename "$0"`
-IMAGES_FOLDER_DIR=$1
+IMAGES_DIR=$1
 SCENARIO=$2
 ARCH=$3
 
-if [[ -z $IMAGES_FOLDER_DIR ]]; then usage; fi;
+if [[ -z $IMAGES_DIR ]]; then usage; fi;
 
 set -ex
-mkdir -p ${IMAGES_FOLDER_DIR}
+mkdir -p ${IMAGES_DIR}
 
 
 ####################
@@ -29,12 +29,12 @@ mkdir -p ${IMAGES_FOLDER_DIR}
 # These images should be present in Functest for the tests to work
 
 # Functest:
-wget -nc ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-disk.img -P ${IMAGES_FOLDER_DIR}
-wget -nc ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-lxc.tar.gz -P ${IMAGES_FOLDER_DIR}
+wget -nc ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-disk.img -P ${IMAGES_DIR}
+wget -nc ${CIRROS_REPO_URL}/${CIRROS_X86_64_TAG}/cirros-${CIRROS_X86_64_TAG}-x86_64-lxc.tar.gz -P ${IMAGES_DIR}
 
 # SNAPS:
-wget -nc http://uec-images.ubuntu.com/releases/trusty/14.04/ubuntu-14.04-server-cloudimg-amd64-disk1.img -P ${IMAGES_FOLDER_DIR}
-wget -nc http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 -P ${IMAGES_FOLDER_DIR}
+wget -nc http://uec-images.ubuntu.com/releases/trusty/14.04/ubuntu-14.04-server-cloudimg-amd64-disk1.img -P ${IMAGES_DIR}
+wget -nc http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 -P ${IMAGES_DIR}
 
 
 ###################
@@ -44,19 +44,24 @@ wget -nc http://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qc
 
 # SDNVPN (odl-bgpvpn scenarios):
 if [[ ${SCENARIO} == *"bgpvpn"* ]]; then
-    wget -nc http://artifacts.opnfv.org/sdnvpn/ubuntu-16.04-server-cloudimg-amd64-disk1.img -P ${IMAGES_FOLDER_DIR}
+    wget -nc http://artifacts.opnfv.org/sdnvpn/ubuntu-16.04-server-cloudimg-amd64-disk1.img -P ${IMAGES_DIR}
 fi
 
 # ONOS (onos-sfc scenarios):
 if [[ ${SCENARIO} == *"onos-sfc"* ]]; then
-    wget -nc http://artifacts.opnfv.org/onosfw/images/firewall_block_image.img -P ${IMAGES_FOLDER_DIR}
+    wget -nc http://artifacts.opnfv.org/onosfw/images/firewall_block_image.img -P ${IMAGES_DIR}
 fi
 
 if [[ ${ARCH} == "arm" ]] || [[ ${ARCH} == "aarch64" ]]; then
     # ARM (aarch64 cirros images):
-    wget -nc ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-disk.img -P ${IMAGES_FOLDER_DIR}
-    wget -nc ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-initramfs -P ${IMAGES_FOLDER_DIR}
-    wget -nc ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-kernel -P ${IMAGES_FOLDER_DIR}
+    wget -nc ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-disk.img -P ${IMAGES_DIR}
+    wget -nc ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-initramfs -P ${IMAGES_DIR}
+    wget -nc ${CIRROS_REPO_URL}/daily/20${CIRROS_AARCH64_TAG}/cirros-d${CIRROS_AARCH64_TAG}-aarch64-kernel -P ${IMAGES_DIR}
+fi
+
+# PARSER
+if [[ ! ${SCENARIO} == *"noha"* ]]; then
+    wget -nc http://download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img -P ${IMAGES_DIR}
 fi
 
 set +ex
