fd8ba7de8b51ca5834d820725600ab3fb03b60bb
Linda Wang <wangwulin@huawei.com>
2017-07-27 01:42:51 +0000

    Omit space line in openrc file
    For alpine testing in compass OSA, sourcing openrc failed due to space
    lines included.
    
    Change-Id: I23166090a2fa7c2ce0aa03be581231cbb3cea34a
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- functest/utils/openstack_utils.py
+++ functest/utils/openstack_utils.py
@@ -117,13 +117,15 @@ def get_credentials(other_creds={}):
 def source_credentials(rc_file):
     with open(rc_file, "r") as f:
         for line in f:
-            var = line.rstrip('"\n').replace('export ', '').split("=")
+            var = (line.rstrip('"\n').replace('export ', '').split("=")
+                   if line.startswith('export') else None)
             # The two next lines should be modified as soon as rc_file
             # conforms with common rules. Be aware that it could induce
             # issues if value starts with '
-            key = re.sub(r'^["\' ]*|[ \'"]*$', '', var[0])
-            value = re.sub(r'^["\' ]*|[ \'"]*$', '', "".join(var[1:]))
-            os.environ[key] = value
+            if var:
+                key = re.sub(r'^["\' ]*|[ \'"]*$', '', var[0])
+                value = re.sub(r'^["\' ]*|[ \'"]*$', '', "".join(var[1:]))
+                os.environ[key] = value
 
 
 def get_credentials_for_rally():
