7bc0ef7a9edee5e47d1766625e5b075cedc2a6e1
Linda Wang <wangwulin@huawei.com>
2017-07-29 03:37:46 +0000

    Restrict ping only once in ping.sh
    Otherwise, it makes no sense to configure ping_timeout as
    200 times.
    
    Change-Id: I6e15f5149f24a4316f6d5b70bcda9a46418d1522
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- functest/opnfv_tests/openstack/vping/ping.sh
+++ functest/opnfv_tests/openstack/vping/ping.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-while true; do
- ping -c 1 $1 2>&1 >/dev/null
- RES=$?
- if [ "Z$RES" = "Z0" ] ; then
-  echo 'vPing OK'
- break
- else
-  echo 'vPing KO'
- fi
- sleep 1
-done
+
+ping -c 1 $1 2>&1 >/dev/null
+RES=$?
+if [ "Z$RES" = "Z0" ] ; then
+    echo 'vPing OK'
+else
+    echo 'vPing KO'
+fi
