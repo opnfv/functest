30d0c784e0c7410ec3e73388d3bb8e71e2938c99
Linda Wang <wangwulin@huawei.com>
2017-08-10 02:37:56 +0000

    Add/Update tempest.conf via a specific file
    Provides a convenient way to configure all params which users required.
    
    JIRA: FUNCTEST-860
    
    Change-Id: Iadfb704285f94530e1023ee780138f22dd672d33
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- functest/tests/unit/vnf/ims/test_cloudify_ims.py
+++ functest/tests/unit/vnf/ims/test_cloudify_ims.py
@@ -79,8 +79,11 @@ class CloudifyImsTesting(unittest.TestCase):
     @mock.patch('snaps.openstack.create_image.OpenStackImage.create')
     def test_prepare_bad_auth_url(self, *args):
         with self.assertRaises(Exception):
-            self.ims_vnf.prepare()
-        args[0].assert_not_called()
+            self.ims_vnf.image_creator(
+                OSCreds(username='user', password='pass', auth_url='url',
+                        project_name='project', identity_api_version=3),
+                mock.Mock())
+            args[0].assert_not_called()
 
     def test_prepare_missing_param(self):
         with self.assertRaises(vnf.VnfPreparationException):
