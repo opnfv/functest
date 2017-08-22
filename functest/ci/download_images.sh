5aac3d73b0e568c39a53b6ebeeef9eeaa85ea673
Tim Rozet <trozet@redhat.com>
2017-08-22 15:16:56 -0400

    Fixes image download script
    Fixes Include:
     - Fix previous behavior where multiple copies of the same file would be
       downloaded as blah.qcow.1, blah.qcow.2 and not overwrite original
       file.
     - Fix issue where files were always downloaded, even when they were not
       different from server version.
    
    Change-Id: I9191847a3f80045329c160e86655cc0a0e80831c
    Signed-off-by: Tim Rozet <trozet@redhat.com>

--- functest/ci/download_images.sh
+++ functest/ci/download_images.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-cat << EOF  | wget -i - -P ${1:-/home/opnfv/functest/images}
-http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img
-https://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img
-https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
-https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
-http://repository.cloudifysource.org/cloudify/4.0.1/sp-release/cloudify-manager-premium-4.0.1.qcow2
-http://marketplace.openbaton.org:8082/api/v1/images/52e2ccc0-1dce-4663-894d-28aab49323aa/img
-http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
-http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-lxc.tar.gz
-http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-disk.img
-http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-initramfs
-http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-kernel
-EOF
+set -e
+set -x
 
-exit $?
+prefix=${1:-/home/opnfv/functest/images}
+
+images="\
+http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-disk.img \
+https://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img \
+https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2 \
+https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img \
+http://repository.cloudifysource.org/cloudify/4.0.1/sp-release/cloudify-manager-premium-4.0.1.qcow2 \
+http://marketplace.openbaton.org:8082/api/v1/images/52e2ccc0-1dce-4663-894d-28aab49323aa/img \
+http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img \
+http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-lxc.tar.gz \
+http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-disk.img \
+http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-initramfs \
+http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-kernel"
+
+pushd $prefix
+for image_url in $images; do
+  wget -N $image_url
+done
+popd
