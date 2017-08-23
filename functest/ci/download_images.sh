6e942d3b97161666c4ec3bd571dd50e5ca3dcb59
Cédric Ollivier <cedric.ollivier@orange.com>
2017-08-23 20:20:44 +0200

    Download images from marketplace.openbaton.org:8082
    LF admins opened port 8082.
    
    Change-Id: I57a2beff32f0a346855968c4229dae4487ccbd85
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/ci/download_images.sh
+++ functest/ci/download_images.sh
@@ -10,6 +10,7 @@ https://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloud
 https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
 https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
 http://repository.cloudifysource.org/cloudify/4.0.1/sp-release/cloudify-manager-premium-4.0.1.qcow2
+http://marketplace.openbaton.org:8082/api/v1/images/52e2ccc0-1dce-4663-894d-28aab49323aa/img
 http://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
 http://download.cirros-cloud.net/0.3.5/cirros-0.3.5-x86_64-lxc.tar.gz
 http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-disk.img
@@ -18,9 +19,6 @@ http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-kernel
 https://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-arm64-uefi1.img
 http://cloud.centos.org/altarch/7/images/aarch64/CentOS-7-aarch64-GenericCloud.qcow2.xz
 EOF
-
-# vnf requires also the next image which cannot be downloaded by jjobs
-# http://marketplace.openbaton.org:8082/api/v1/images/52e2ccc0-1dce-4663-894d-28aab49323aa/img
 
 xz --decompress ${1:-/home/opnfv/functest/images}/CentOS-7-aarch64-GenericCloud.qcow2.xz
 
