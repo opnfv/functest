dfcad47e113dbda718075f603acf3567b59501dd
Linda Wang <wangwulin@huawei.com>
2017-08-30 09:00:27 +0000

    Disable urllib3 warnings
    When running in compass OSA, lots of SubjectAltNameWarnings are
    shown in console, so just disable the warnings here.
    See https://github.com/shazow/urllib3/issues/497 for details
    
    Change-Id: Ic22cf8e2babc10c9d55a7ed46b841312a4e430a7
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- functest/ci/prepare_env.py
+++ functest/ci/prepare_env.py
@@ -17,6 +17,7 @@ import subprocess
 import sys
 import fileinput
 
+import requests.packages.urllib3
 import yaml
 
 from functest.ci import check_deployment
@@ -37,14 +38,14 @@ pod_arch = os.getenv("POD_ARCH", None)
 arch_filter = ['aarch64']
 
 CONFIG_FUNCTEST_PATH = pkg_resources.resource_filename(
-            'functest', 'ci/config_functest.yaml')
+    'functest', 'ci/config_functest.yaml')
 CONFIG_PATCH_PATH = pkg_resources.resource_filename(
-            'functest', 'ci/config_patch.yaml')
+    'functest', 'ci/config_patch.yaml')
 CONFIG_AARCH64_PATCH_PATH = pkg_resources.resource_filename(
-            'functest', 'ci/config_aarch64_patch.yaml')
+    'functest', 'ci/config_aarch64_patch.yaml')
 RALLY_CONF_PATH = "/etc/rally/rally.conf"
 RALLY_AARCH64_PATCH_PATH = pkg_resources.resource_filename(
-            'functest', 'ci/rally_aarch64_patch.conf')
+    'functest', 'ci/rally_aarch64_patch.conf')
 
 
 class PrepareEnvParser(object):
@@ -358,6 +359,7 @@ def prepare_env(**kwargs):
 def main():
     logging.config.fileConfig(pkg_resources.resource_filename(
         'functest', 'ci/logging.ini'))
+    requests.packages.urllib3.disable_warnings()
     parser = PrepareEnvParser()
     args = parser.parse_args(sys.argv[1:])
     return prepare_env(**args)
