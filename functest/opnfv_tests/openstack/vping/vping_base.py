b7f390c91994cbcebac60e55f32a543cd4d46c1c
Morgan Richomme <morgan.richomme@orange.com>
2017-10-02 17:11:13 +0200

    Bug fix: vping_userdata test status was not properly reported
    vping_base is calling vping_userdata
    vping_user data could return only EX_TESTCASE_FAILED or EX_OK
    by default vping_base always returns EX_OK (based on self.result=100..)
    it returned EX_RUN_ERROR only if upper class report EX_RUN_ERROR
    
    that is why vping_userdata was always PASS even if the trace "Ping OK"
    cannot be retrieved from the Nova console
    
    it seems that when running the vping_userdata, we systematically got
     Failed to read iid from metadata
    
    Change-Id: If72441dd93bffada5c7a5399c87db68d7ebeebed
    Signed-off-by: Morgan Richomme <morgan.richomme@orange.com>
    (cherry picked from commit e36a14f6db0bc5ac93469bb553810de506f29bef)

--- functest/opnfv_tests/openstack/vping/vping_base.py
+++ functest/opnfv_tests/openstack/vping/vping_base.py
@@ -165,7 +165,7 @@ class VPingBase(testcase.TestCase):
         else:
             raise Exception('VMs never became active')
 
-        if result == testcase.TestCase.EX_RUN_ERROR:
+        if result != testcase.TestCase.EX_OK:
             return testcase.TestCase.EX_RUN_ERROR
 
         self.stop_time = time.time()
