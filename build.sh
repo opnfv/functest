314358464c8efa3752d21efb4d96b37bc5281982
Cédric Ollivier <cedric.ollivier@orange.com>
2017-10-11 05:29:11 +0200

    Allows building aarch64 images
    It must be noted that Docker automated build cannot create these
    images as the prerequisites are unmet to cross-compile [1].
    
    It also publishes a travis config file.
    
    [1] https://www.kernel.org/doc/html/v4.11/admin-guide/binfmt-misc.html
    
    Change-Id: I11fdef410cc40989132205a098410175f789fdef
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 repo=${repo:-opnfv}
-dirs="\
+x86_64_dirs=${x86_64_dirs-"\
 docker/core \
 docker/healthcheck \
 docker/smoke \
@@ -11,14 +11,23 @@ docker/features \
 docker/components \
 docker/vnf \
 docker/parser \
-docker/restapi"
+docker/restapi"}
+aarch64_dirs=${aarch64_dirs-$(echo "${x86_64_dirs}" | sed -e "s|docker/vnf||" \
+    -e "s|docker/restapi||")}
 
-(cd docker && docker build -t "${repo}/functest" .)
-docker push "${repo}/functest"
-
-for dir in ${dirs}; do
-    (cd ${dir} && docker build -t "${repo}/functest-${dir##**/}" .)
-    docker push "${repo}/functest-${dir##**/}"
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core_x86_64|g" {} +
+for dir in ${x86_64_dirs}; do
+    (cd "${dir}" && docker build -t "${repo}/functest-${dir##**/}_x86_64" .)
+    docker push "${repo}/functest-${dir##**/}_x86_64"
 done
+find . -name Dockerfile -exec git checkout {} +
+
+find . -name Dockerfile -exec sed -i -e "s|alpine:3.6|multiarch/alpine:aarch64-v3.6|g" {} +
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core_aarch64|g" {} +
+for dir in ${aarch64_dirs}; do
+    (cd "${dir}" && docker build -t "${repo}/functest-${dir##**/}_aarch64" .)
+    docker push "${repo}/functest-${dir##**/}_aarch64"
+done
+find . -name Dockerfile -exec git checkout {} +
 
 exit $?
