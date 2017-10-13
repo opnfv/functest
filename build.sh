fed72336e3b50b8441f233a2f122e482ee83ae8c
Cédric Ollivier <cedric.ollivier@orange.com>
2017-10-13 10:38:38 +0200

    Conform with Docker ARCH names
    It switches Docker tags:
      - x86_64 -> amd64
      - aarch64 -> arm64
    
    Conflicts:
        .travis.yml
        build.sh
    
    Change-Id: I05575dd728e7502a891a3ee3015f81ac8e3f7a92
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit a8acc48148313229b4e6771374ece9256d75b26c)

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 repo=${repo:-opnfv}
-x86_64_dirs=${x86_64_dirs-"\
+amd64_dirs=${amd64_dirs-"\
 docker/core \
 docker/healthcheck \
 docker/smoke \
@@ -12,21 +12,21 @@ docker/components \
 docker/vnf \
 docker/parser \
 docker/restapi"}
-aarch64_dirs=${aarch64_dirs-$(echo "${x86_64_dirs}" | sed -e "s|docker/vnf||" \
+arm64_dirs=${arm64_dirs-$(echo "${amd64_dirs}" | sed -e "s|docker/vnf||" \
     -e "s|docker/restapi||")}
 
-find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core:euphrates|${repo}/functest-core:x86_64-euphrates|g" {} +
-for dir in ${x86_64_dirs}; do
-    (cd "${dir}" && docker build -t "${repo}/functest-${dir##**/}:x86_64-euphrates" .)
-    docker push "${repo}/functest-${dir##**/}:x86_64-euphrates"
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core:euphrates|${repo}/functest-core:amd64-euphrates|g" {} +
+for dir in ${amd64_dirs}; do
+    (cd "${dir}" && docker build -t "${repo}/functest-${dir##**/}:amd64-euphrates" .)
+    docker push "${repo}/functest-${dir##**/}:amd64-euphrates"
 done
 find . -name Dockerfile -exec git checkout {} +
 
-find . -name Dockerfile -exec sed -i -e "s|alpine:3.6|multiarch/alpine:aarch64-v3.6|g" {} +
-find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core:euphrates|${repo}/functest-core:aarch64-euphrates|g" {} +
-for dir in ${aarch64_dirs}; do
-    (cd "${dir}" && docker build -t "${repo}/functest-${dir##**/}:aarch64-euphrates" .)
-    docker push "${repo}/functest-${dir##**/}:aarch64-euphrates"
+find . -name Dockerfile -exec sed -i -e "s|alpine:3.6|multiarch/alpine:arm64-v3.6|g" {} +
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core:euphrates|${repo}/functest-core:arm64-euphrates|g" {} +
+for dir in ${arm64_dirs}; do
+    (cd "${dir}" && docker build -t "${repo}/functest-${dir##**/}:arm64-euphrates" .)
+    docker push "${repo}/functest-${dir##**/}:arm64-euphrates"
 done
 find . -name Dockerfile -exec git checkout {} +
 
