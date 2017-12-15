7fb64b3fd8ce4c8abdfb1c229ceb13faa800018f
Linda Wang <wangwulin@huawei.com>
2017-12-15 02:57:26 +0000

    Inject all envs in openrc into CONST
    After "env_prepare" is removed, some specific vars in openrc
    (eg,."EXTERNAL_NETWORK") loaded to os.environ should be injected
    into CONST in one python process.
    
    Change-Id: Iaa5c6047033dc2a30c533174e2016111759aec31
    Signed-off-by: Linda Wang <wangwulin@huawei.com>

--- functest/utils/openstack_utils.py
+++ functest/utils/openstack_utils.py
@@ -127,6 +127,7 @@ def source_credentials(rc_file):
                 key = re.sub(r'^["\' ]*|[ \'"]*$', '', var[0])
                 value = re.sub(r'^["\' ]*|[ \'"]*$', '', "".join(var[1:]))
                 os.environ[key] = value
+                CONST.__setattr__(key, value)
 
 
 def get_session_auth(other_creds={}):
