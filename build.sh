b18dcf6d7aa4a4067e7a5d62b1ee0232e4faccb9
Cédric Ollivier <cedric.ollivier@orange.com>
2017-12-23 12:41:10 +0100

    Add Docker build opts
    Change-Id: I91fcb833b6b6216338b4fcb888f093a2086264a7
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -14,24 +14,19 @@ docker/parser \
 docker/restapi"}
 arm64_dirs=${arm64_dirs-$(echo "${amd64_dirs}" | sed -e "s|docker/vnf||" \
     -e "s|docker/restapi||")}
+build_opts="--pull=true --no-cache --force-rm=true"
 
 find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core:amd64-latest|g" {} +
-if [[ ${amd64_dirs} != *"docker/core"* ]]; then
-    docker pull "${repo}/functest-core:amd64-latest"
-fi
 for dir in ${amd64_dirs}; do
-    (cd "${dir}" && docker build --no-cache -t "${repo}/functest-${dir##**/}:amd64-latest" .)
+    (cd "${dir}" && docker build ${build_opts} -t "${repo}/functest-${dir##**/}:amd64-latest" .)
     docker push "${repo}/functest-${dir##**/}:amd64-latest"
 done
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i -e "s|alpine:3.6|multiarch/alpine:arm64-v3.6|g" {} +
 find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core:arm64-latest|g" {} +
-if [[ ${arm64_dirs} != *"docker/core"* ]]; then
-    docker pull "${repo}/functest-core:arm64-latest"
-fi
 for dir in ${arm64_dirs}; do
-    (cd "${dir}" && docker build --no-cache -t "${repo}/functest-${dir##**/}:arm64-latest" .)
+    (cd "${dir}" && docker build ${build_opts} -t "${repo}/functest-${dir##**/}:arm64-latest" .)
     docker push "${repo}/functest-${dir##**/}:arm64-latest"
 done
 find . -name Dockerfile -exec git checkout {} +
