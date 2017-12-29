7e7b5ab80efa5c904a6bbb1849e6969ecd860a60
Jose Lausuch <jalausuch@suse.com>
2017-12-29 12:37:11 +0100

    Move push_results and get_version to TestCase
    Change-Id: Ie072c675890d0ae2e63619c599d2684a8c7d762d
    Signed-off-by: Jose Lausuch <jalausuch@suse.com>

--- functest/core/testcase.py
+++ functest/core/testcase.py
@@ -9,12 +9,17 @@
 
 """Define the parent class of all Functest TestCases."""
 
+
+import json
 import logging
 import os
+import re
 
-import functest.utils.functest_utils as ft_utils
+from functest.utils.constants import CONST
 
+from datetime import datetime as dt
 import prettytable
+import requests
 
 
 __author__ = "Cedric Ollivier <cedric.ollivier@orange.com>"
@@ -62,6 +67,28 @@ class TestCase(object):
         except AssertionError:
             self.__logger.error("We cannot print invalid objects")
             return super(TestCase, self).__str__()
+
+    @staticmethod
+    def get_version():
+        """
+        Get version
+        """
+        # Use the build tag to retrieve the version
+        # By default version is unknown
+        # if launched through CI the build tag has the following format
+        # jenkins-<project>-<installer>-<pod>-<job>-<branch>-<id>
+        # e.g. jenkins-functest-fuel-opnfv-jump-2-daily-master-190
+        # jenkins-functest-fuel-baremetal-weekly-master-8
+        # use regex to match branch info
+        rule = "(dai|week)ly-(.+?)-[0-9]*"
+        build_tag = CONST.__getattribute__('BUILD_TAG')
+        if not build_tag:
+            build_tag = 'none'
+        m = re.search(rule, build_tag)
+        if m:
+            return m.group(2)
+        else:
+            return "unknown"
 
     def get_duration(self):
         """Return the duration of the test case.
@@ -163,20 +190,82 @@ class TestCase(object):
             assert self.case_name
             assert self.start_time
             assert self.stop_time
+            installer = os.environ['INSTALLER_TYPE']
+            scenario = os.environ['DEPLOY_SCENARIO']
+            pod_name = os.environ['NODE_NAME']
+            build_tag = os.environ['BUILD_TAG']
+            version = TestCase.get_version()
+            test_start = dt.fromtimestamp(self.start_time).strftime(
+                '%Y-%m-%d %H:%M:%S')
+            test_stop = dt.fromtimestamp(self.stop_time).strftime(
+                '%Y-%m-%d %H:%M:%S')
+            if (hasattr(CONST, 'TEST_DB_URL')):
+                url = CONST.__getattribute__('TEST_DB_URL')
+            else:
+                url = CONST.__getattribute__("results_test_db_url")
             pub_result = 'PASS' if self.is_successful(
                 ) == TestCase.EX_OK else 'FAIL'
-            if ft_utils.push_results_to_db(
-                    self.project_name, self.case_name, self.start_time,
-                    self.stop_time, pub_result, self.details):
+
+            params = {"project_name": self.project_name,
+                      "case_name": self.case_name,
+                      "pod_name": pod_name,
+                      "installer": installer,
+                      "version": version,
+                      "scenario": scenario,
+                      "criteria": self.result,
+                      "build_tag": build_tag,
+                      "start_date": test_start,
+                      "stop_date": test_stop,
+                      "details": self.details}
+        except Exception:  # pylint: disable=broad-except
+            self.__logger.error("Problem while getting test parameters.")
+            return TestCase.EX_PUSH_TO_DB_ERROR
+
+        error = None
+        headers = {'Content-Type': 'application/json'}
+        try:
+            r = requests.post(url, data=json.dumps(params, sort_keys=True),
+                              headers=headers)
+            r.raise_for_status()
+        except requests.RequestException as exc:
+            if 'r' in locals():
+                error = ("Pushing Result to DB(%s) failed: %s" %
+                         (r.url, r.content))
+            else:
+                error = ("Pushing Result to DB(%s) failed: %s" % (url, exc))
+        except Exception as e:
+            error = ("Error [push_results_to_db("
+                     "DB: '%(db)s', "
+                     "project: '%(project)s', "
+                     "case: '%(case)s', "
+                     "pod: '%(pod)s', "
+                     "version: '%(v)s', "
+                     "scenario: '%(s)s', "
+                     "criteria: '%(c)s', "
+                     "build_tag: '%(t)s', "
+                     "details: '%(d)s')]: "
+                     "%(error)s" %
+                     {
+                         'db': url,
+                         'project': self.project_name,
+                         'case': self.case_name,
+                         'pod': pod_name,
+                         'v': version,
+                         's': scenario,
+                         'c': self.result,
+                         't': build_tag,
+                         'd': self.details,
+                         'error': e
+                     })
+        finally:
+            if error:
+                self.__logger.error("The results cannot be "
+                                    "pushed to DB. %s" % error)
+                return TestCase.EX_PUSH_TO_DB_ERROR
+            else:
                 self.__logger.info(
                     "The results were successfully pushed to DB")
                 return TestCase.EX_OK
-            else:
-                self.__logger.error("The results cannot be pushed to DB")
-                return TestCase.EX_PUSH_TO_DB_ERROR
-        except Exception:  # pylint: disable=broad-except
-            self.__logger.exception("The results cannot be pushed to DB")
-            return TestCase.EX_PUSH_TO_DB_ERROR
 
     def clean(self):
         """Clean the resources.
