1566ee88053fa72e4161f762a1c9ec38da94930e
Juha Kosonen <juha.kosonen@nokia.com>
2018-02-12 16:54:53 +0200

    Enable invalid-name warnings in tempest modules
    Do not suppress pylint invalid-name warnings but conform the naming
    convention.
    
    Change-Id: I324d25b3081fe8a366249559b343c1b9cacadb21
    Signed-off-by: Juha Kosonen <juha.kosonen@nokia.com>

--- functest/tests/unit/openstack/tempest/test_conf_utils.py
+++ functest/tests/unit/openstack/tempest/test_conf_utils.py
@@ -5,7 +5,7 @@
 # which accompanies this distribution, and is available at
 # http://www.apache.org/licenses/LICENSE-2.0
 
-# pylint: disable=invalid-name,missing-docstring
+# pylint: disable=missing-docstring
 
 import logging
 import unittest
@@ -32,7 +32,7 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
                 return_value=None)
     @mock.patch('snaps.openstack.utils.deploy_utils.create_image',
                 return_value=mock.Mock())
-    def test_create_tempest_resources_missing_network_dic(self, *mock_args):
+    def test_create_res_missing_net_dic(self, *mock_args):
         # pylint: disable=unused-argument
         tempest_resources = tempest.TempestResourcesManager(
             os_creds=self.os_creds)
@@ -49,7 +49,7 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
                 return_value=mock.Mock())
     @mock.patch('snaps.openstack.utils.deploy_utils.create_image',
                 return_value=None)
-    def test_create_tempest_resources_missing_image(self, *mock_args):
+    def test_create_res_missing_image(self, *mock_args):
         # pylint: disable=unused-argument
         tempest_resources = tempest.TempestResourcesManager(
             os_creds=self.os_creds)
@@ -69,7 +69,7 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
                 return_value=mock.Mock())
     @mock.patch('snaps.openstack.create_flavor.OpenStackFlavor.create',
                 return_value=None)
-    def test_create_tempest_resources_missing_flavor(self, *mock_args):
+    def test_create_res_missing_flavor(self, *mock_args):
         # pylint: disable=unused-argument
         tempest_resources = tempest.TempestResourcesManager(
             os_creds=self.os_creds)
@@ -132,7 +132,7 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
                 'create_verifier', return_value=mock.Mock())
     @mock.patch('functest.opnfv_tests.openstack.tempest.conf_utils.'
                 'create_rally_deployment', return_value=mock.Mock())
-    def test_get_verifier_id_missing_verifier(self, mock_rally, mock_tempest):
+    def test_get_verif_id_missing_verif(self, mock_rally, mock_tempest):
         # pylint: disable=unused-argument
         CONST.__setattr__('tempest_verifier_name', 'test_verifier_name')
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
@@ -161,7 +161,7 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
             self.assertEqual(conf_utils.get_verifier_id(),
                              'test_deploy_id')
 
-    def test_get_verifier_deployment_id_missing_rally(self):
+    def test_get_depl_id_missing_rally(self):
         CONST.__setattr__('tempest_verifier_name', 'test_deploy_name')
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.subprocess.Popen') as mock_popen, \
@@ -172,7 +172,7 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
             mock_popen.return_value = mock_stdout
             conf_utils.get_verifier_deployment_id()
 
-    def test_get_verifier_deployment_id_default(self):
+    def test_get_depl_id_default(self):
         CONST.__setattr__('tempest_verifier_name', 'test_deploy_name')
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.subprocess.Popen') as mock_popen:
@@ -184,59 +184,61 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
             self.assertEqual(conf_utils.get_verifier_deployment_id(),
                              'test_deploy_id')
 
-    def test_get_verifier_repo_dir_default(self):
+    def test_get_verif_repo_dir_default(self):
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.os.path.join',
                         return_value='test_verifier_repo_dir'), \
             mock.patch('functest.opnfv_tests.openstack.tempest.'
-                       'conf_utils.get_verifier_id') as m:
+                       'conf_utils.get_verifier_id') as mock_get_id:
             self.assertEqual(conf_utils.get_verifier_repo_dir(''),
                              'test_verifier_repo_dir')
-            self.assertTrue(m.called)
+            self.assertTrue(mock_get_id.called)
 
-    def test_get_verifier_deployment_dir_default(self):
+    def test_get_depl_dir_default(self):
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.os.path.join',
                         return_value='test_verifier_repo_dir'), \
             mock.patch('functest.opnfv_tests.openstack.tempest.'
-                       'conf_utils.get_verifier_id') as m1, \
+                       'conf_utils.get_verifier_id') as mock_get_vid, \
             mock.patch('functest.opnfv_tests.openstack.tempest.'
-                       'conf_utils.get_verifier_deployment_id') as m2:
+                       'conf_utils.get_verifier_deployment_id') \
+                as mock_get_did:
             self.assertEqual(conf_utils.get_verifier_deployment_dir('', ''),
                              'test_verifier_repo_dir')
-            self.assertTrue(m1.called)
-            self.assertTrue(m2.called)
+            self.assertTrue(mock_get_vid.called)
+            self.assertTrue(mock_get_did.called)
 
-    def test_backup_tempest_config_default(self):
+    def test_backup_config_default(self):
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.os.path.exists',
                         return_value=False), \
             mock.patch('functest.opnfv_tests.openstack.tempest.'
-                       'conf_utils.os.makedirs') as m1, \
+                       'conf_utils.os.makedirs') as mock_makedirs, \
             mock.patch('functest.opnfv_tests.openstack.tempest.'
-                       'conf_utils.shutil.copyfile') as m2:
+                       'conf_utils.shutil.copyfile') as mock_copyfile:
             conf_utils.backup_tempest_config('test_conf_file')
-            self.assertTrue(m1.called)
-            self.assertTrue(m2.called)
+            self.assertTrue(mock_makedirs.called)
+            self.assertTrue(mock_copyfile.called)
 
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.os.path.exists',
                         return_value=True), \
             mock.patch('functest.opnfv_tests.openstack.tempest.'
-                       'conf_utils.shutil.copyfile') as m2:
+                       'conf_utils.shutil.copyfile') as mock_copyfile:
             conf_utils.backup_tempest_config('test_conf_file')
-            self.assertTrue(m2.called)
+            self.assertTrue(mock_copyfile.called)
 
-    def test_configure_tempest_default(self):
+    def test_conf_tempest_def(self):
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.configure_verifier',
                         return_value='test_conf_file'), \
             mock.patch('functest.opnfv_tests.openstack.tempest.'
-                       'conf_utils.configure_tempest_update_params') as m1:
+                       'conf_utils.configure_tempest_update_params')\
+                as mock_upd:
             conf_utils.configure_tempest('test_dep_dir')
-            self.assertTrue(m1.called)
+            self.assertTrue(mock_upd.called)
 
-    def test_configure_tempest_defcore_default(self):
+    def test_conf_tempest_defcore_def(self):
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.configure_verifier',
                         return_value='test_conf_file'), \
@@ -268,7 +270,7 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
             self.assertTrue(mread.called)
             self.assertTrue(mwrite.called)
 
-    def test_generate_test_accounts_file_default(self):
+    def test_gen_test_accounts_file_def(self):
         with mock.patch("__builtin__.open", mock.mock_open()), \
             mock.patch('functest.opnfv_tests.openstack.tempest.conf_utils.'
                        'yaml.dump') as mock_dump:
@@ -297,27 +299,27 @@ class OSTempestConfUtilsTesting(unittest.TestCase):
             self.assertTrue(mread.called)
             self.assertTrue(mwrite.called)
 
-    def test_configure_tempest_update_params_missing_image_id(self):
+    def test_upd_missing_image_id(self):
         self._test_missing_param(('compute', 'image_ref', 'test_image_id'),
                                  'test_image_id', None)
 
-    def test_configure_tempest_update_params_missing_image_id_alt(self):
+    def test_upd_missing_image_id_alt(self):
         conf_utils.IMAGE_ID_ALT = 'test_image_id_alt'
         self._test_missing_param(('compute', 'image_ref_alt',
                                   'test_image_id_alt'), None, None)
 
-    def test_configure_tempest_update_params_missing_flavor_id(self):
+    def test_upd_missing_flavor_id(self):
         CONST.__setattr__('tempest_use_custom_flavors', 'True')
         self._test_missing_param(('compute', 'flavor_ref', 'test_flavor_id'),
                                  None, 'test_flavor_id')
 
-    def test_configure_tempest_update_params_missing_flavor_id_alt(self):
+    def test_upd_missing_flavor_id_alt(self):
         CONST.__setattr__('tempest_use_custom_flavors', 'True')
         conf_utils.FLAVOR_ID_ALT = 'test_flavor_id_alt'
         self._test_missing_param(('compute', 'flavor_ref_alt',
                                   'test_flavor_id_alt'), None, None)
 
-    def test_configure_verifier_missing_temp_conf_file(self):
+    def test_verif_missing_conf_file(self):
         with mock.patch('functest.opnfv_tests.openstack.tempest.'
                         'conf_utils.os.path.isfile',
                         return_value=False), \
