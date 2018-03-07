97088b8ebd129ea937120aad171f304e2d89799e
Cédric Ollivier <cedric.ollivier@orange.com>
2018-03-07 10:26:26 +0100

    Add neutron and glance policy.json for patrole
    It only runs tests about neutron and glance because their policy.json
    can be still downloaded. Installers could override them via docker
    volumes if needed.
    
    Several tests about provider networks are currently excluded because
    GRE is selected.
    
    Change-Id: Ic4e2274e3f53e66be0174e117993e8c307b96bf9
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/opnfv_tests/openstack/patrole/patrole.py
+++ functest/opnfv_tests/openstack/patrole/patrole.py
@@ -13,10 +13,12 @@ import logging
 import os
 import time
 
+from xtesting.core import testcase
+
 from functest.opnfv_tests.openstack.snaps import snaps_utils
 from functest.opnfv_tests.openstack.tempest import conf_utils
 from functest.opnfv_tests.openstack.tempest import tempest
-from xtesting.core import testcase
+from functest.utils import config
 
 
 class Patrole(tempest.TempestCommon):
@@ -27,13 +29,18 @@ class Patrole(tempest.TempestCommon):
         if "case_name" not in kwargs:
             kwargs["case_name"] = 'patrole'
         super(Patrole, self).__init__(**kwargs)
-        self.mode = "^patrole_tempest_plugin."
+        self.mode = ("'(?!.*test_networks_multiprovider_rbac)"
+                     "(?=patrole_tempest_plugin.tests.api.(image|network))'")
+        self.res_dir = os.path.join(
+            getattr(config.CONF, 'dir_results'), 'patrole')
+        self.raw_list = os.path.join(self.res_dir, 'test_raw_list.txt')
+        self.list = os.path.join(self.res_dir, 'test_list.txt')
 
     def run(self, **kwargs):
         self.start_time = time.time()
         try:
-            if not os.path.exists(conf_utils.TEMPEST_RESULTS_DIR):
-                os.makedirs(conf_utils.TEMPEST_RESULTS_DIR)
+            if not os.path.exists(self.res_dir):
+                os.makedirs(self.res_dir)
             resources = self.resources.create()
             compute_cnt = snaps_utils.get_active_compute_cnt(
                 self.resources.os_creds)
@@ -42,7 +49,8 @@ class Patrole(tempest.TempestCommon):
                 network_name=resources.get("network_name"),
                 image_id=resources.get("image_id"),
                 flavor_id=resources.get("flavor_id"),
-                compute_cnt=compute_cnt)
+                compute_cnt=compute_cnt,
+                role=kwargs.get('role', 'admin'))
             self.generate_test_list(self.verifier_repo_dir)
             self.apply_tempest_blacklist()
             self.run_verifier_tests()
@@ -59,7 +67,7 @@ class Patrole(tempest.TempestCommon):
 
     def configure_tempest_patrole(
             self, deployment_dir, network_name=None, image_id=None,
-            flavor_id=None, compute_cnt=None):
+            flavor_id=None, compute_cnt=None, role='admin'):
         # pylint: disable=too-many-arguments
         """
         Add/update needed parameters into tempest.conf file
@@ -69,11 +77,11 @@ class Patrole(tempest.TempestCommon):
         conf_file = conf_utils.configure_verifier(deployment_dir)
         conf_utils.configure_tempest_update_params(
             conf_file, network_name, image_id, flavor_id, compute_cnt)
-        config = conf_utils.ConfigParser.RawConfigParser()
-        config.read(conf_file)
-        config.set('identity-feature-enabled', 'api_v2', False)
-        config.add_section('rbac')
-        config.set('rbac', 'enable_rbac', True)
-        config.set('rbac', 'rbac_test_role', 'admin')
+        rconfig = conf_utils.ConfigParser.RawConfigParser()
+        rconfig.read(conf_file)
+        rconfig.set('identity-feature-enabled', 'api_v2', False)
+        rconfig.add_section('rbac')
+        rconfig.set('rbac', 'enable_rbac', True)
+        rconfig.set('rbac', 'rbac_test_role', role)
         with open(conf_file, 'wb') as config_file:
-            config.write(config_file)
+            rconfig.write(config_file)
