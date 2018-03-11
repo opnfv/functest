6548623a72119f4f105608565a61526e2c5664a4
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-03-11 23:34:57 +0100

    ci/download_images.sh: Reduce wget dot reporting
    Downloading large images outputs a lot of useless dots to show
    progress. Switch to 1M per dot (from 1K).
    
    Change-Id: I00e3d09ef28e7722798eeadc27649a96398aac32
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- functest/ci/download_images.sh
+++ functest/ci/download_images.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-wget_opts="-N --tries=1 --connect-timeout=30"
+wget_opts="-N --tries=1 --connect-timeout=30 --progress=dot:giga"
 
 cat << EOF  | wget ${wget_opts} -i - -P ${1:-/home/opnfv/functest/images}
 http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img
