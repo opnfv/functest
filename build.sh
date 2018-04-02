ab61783961b107dade0c9bc9f7e405abb3adbc35
Cédric Ollivier <cedric.ollivier@orange.com>
2018-04-02 04:19:37 +0200

    Update Dockerfiles and constraints to Fraser
    Only thirparties which have already created stable/fraser have been
    updated.
    
    Change-Id: Idb937b3ed4896208048b90b705dc3e6db6104b60
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -16,23 +16,23 @@ docker/restapi"}
 arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
-find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core:amd64-latest|g" {} +
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core:amd64-fraser|g" {} +
 for dir in ${amd64_dirs}; do
-    (cd "${dir}" && docker build "${build_opts[@]}" -t "${repo}/functest-${dir##**/}:amd64-latest" .)
-    docker push "${repo}/functest-${dir##**/}:amd64-latest"
-    [ "${dir}" != "docker/core" ] && (docker rmi "${repo}/functest-${dir##**/}:amd64-latest" || true)
+    (cd "${dir}" && docker build "${build_opts[@]}" -t "${repo}/functest-${dir##**/}:amd64-fraser" .)
+    docker push "${repo}/functest-${dir##**/}:amd64-fraser"
+    [ "${dir}" != "docker/core" ] && (docker rmi "${repo}/functest-${dir##**/}:amd64-fraser" || true)
 done
-[ ! -z "${amd64_dirs}" ] && (docker rmi "${repo}/functest-core:amd64-latest" alpine:3.7 || true)
+[ ! -z "${amd64_dirs}" ] && (docker rmi "${repo}/functest-core:amd64-fraser" alpine:3.7 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i -e "s|alpine:3.7|multiarch/alpine:arm64-v3.7|g" {} +
-find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core:arm64-latest|g" {} +
+find . -name Dockerfile -exec sed -i -e "s|opnfv/functest-core|${repo}/functest-core:arm64-fraser|g" {} +
 for dir in ${arm64_dirs}; do
-    (cd "${dir}" && docker build "${build_opts[@]}" -t "${repo}/functest-${dir##**/}:arm64-latest" .)
-    docker push "${repo}/functest-${dir##**/}:arm64-latest"
-    [ "${dir}" != "docker/core" ] && (docker rmi "${repo}/functest-${dir##**/}:arm64-latest" || true)
+    (cd "${dir}" && docker build "${build_opts[@]}" -t "${repo}/functest-${dir##**/}:arm64-fraser" .)
+    docker push "${repo}/functest-${dir##**/}:arm64-fraser"
+    [ "${dir}" != "docker/core" ] && (docker rmi "${repo}/functest-${dir##**/}:arm64-fraser" || true)
 done
-[ ! -z "${arm64_dirs}" ] && (docker rmi "${repo}/functest-core:arm64-latest" multiarch/alpine:arm64-v3.7 || true)
+[ ! -z "${arm64_dirs}" ] && (docker rmi "${repo}/functest-core:arm64-fraser" multiarch/alpine:arm64-v3.7 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 exit $?
