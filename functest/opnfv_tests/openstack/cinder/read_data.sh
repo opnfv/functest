57ba9f998e3496b9caa6490d2aaeb9242f1eaafb
Delia Popescu <delia.popescu@enea.com>
2018-05-10 12:13:08 +0300

    Add cinder_test testcase
    Create 2 instances with Floating IPs
    Attach data volume to instance 1
    Write data on the volume
    Detach volume from instance 1
    Attach volume to instance 2
    Read data from volume
    
    JIRA: FUNCTEST-908
    
    Change-Id: I10efdfb3b7f9d7e6ab5c6a538052437585d52fb0
    Signed-off-by: Delia Popescu <delia.popescu@enea.com>

---
+++ functest/opnfv_tests/openstack/cinder/read_data.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -e
+
+# Copyright (c) 2018 Enea AB and others
+
+# This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+
+VOL_DEV_NAME="$(lsblk -list | tr ' ' '\012'| grep "\\(vdc\\|sdc\\)")"
+
+if [ -n "$VOL_DEV_NAME" ]; then
+    sudo mount /dev/$VOL_DEV_NAME ./volume;
+    if echo ./volume/*new_data*; then
+        echo "Found existing data!";
+    else
+        echo "No data found on the volume!";
+        exit 1
+    fi
+fi
