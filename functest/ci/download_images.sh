2ae7cf70f298c03a4d54072338075b35d6bc5222
Delia Popescu <delia.popescu@enea.com>
2018-05-14 12:03:34 +0300

    Update cirros image version for ARM architecture
    Change-Id: Ia7a65a4f83798cb80ca7470d475c2c2d8c9fb6ea
    Signed-off-by: Delia Popescu <delia.popescu@enea.com>

--- functest/ci/download_images.sh
+++ functest/ci/download_images.sh
@@ -11,9 +11,9 @@ https://cloud.centos.org/centos/7/images/CentOS-7-x86_64-GenericCloud.qcow2
 https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
 http://repository.cloudifysource.org/cloudify/4.0.1/sp-release/cloudify-manager-premium-4.0.1.qcow2
 http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-lxc.tar.gz
-http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-disk.img
-http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-initramfs
-http://download.cirros-cloud.net/daily/20161201/cirros-d161201-aarch64-kernel
+http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-aarch64-disk.img
+http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-aarch64-initramfs
+http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-aarch64-kernel
 https://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-arm64-uefi1.img
 http://cloud.centos.org/altarch/7/images/aarch64/CentOS-7-aarch64-GenericCloud.qcow2.xz
 https://sourceforge.net/projects/ool-opnfv/files/vyos-1.1.7.img
