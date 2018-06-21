702f0cfa36ae66bb6ad8f92d3d9592b06784c9a7
Cédric Ollivier <cedric.ollivier@orange.com>
2018-06-21 18:11:09 +0200

    Add the Barbican test suite
    It eases verifying the functionality of a barbican installation.
    
    Change-Id: Iac2144ae6ba4d61b2b37bb5c7a0359edb3f222a6
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/opnfv_tests/openstack/tempest/tempest.py
+++ functest/opnfv_tests/openstack/tempest/tempest.py
@@ -321,6 +321,19 @@ class TempestNeutronTrunk(TempestCommon):
             rconfig.write(config_file)
 
 
+class TempestBarbican(TempestCommon):
+    """Tempest Barbican testcase implementation."""
+    def __init__(self, **kwargs):
+        if "case_name" not in kwargs:
+            kwargs["case_name"] = 'barbican'
+        TempestCommon.__init__(self, **kwargs)
+        self.mode = "'barbican_tempest_plugin.tests.(api|scenario)'"
+        self.res_dir = os.path.join(
+            getattr(config.CONF, 'dir_results'), 'neutron_trunk')
+        self.raw_list = os.path.join(self.res_dir, 'test_raw_list.txt')
+        self.list = os.path.join(self.res_dir, 'test_list.txt')
+
+
 class TempestSmokeParallel(TempestCommon):
     """Tempest smoke parallel testcase implementation."""
     def __init__(self, **kwargs):
