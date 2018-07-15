38cc424c2a48a14a25b02eea03d70784577753a4
Cédric Ollivier <cedric.ollivier@orange.com>
2018-07-15 15:11:35 +0200

    Define env vars for ODL inputs
    It's linked to "Add default param for ODL password" [1].
    
    [1] https://gerrit.opnfv.org/gerrit/#/c/59811/
    
    Change-Id: Ib88d9371a2b6febb5c1af69ffda9b3c5024999e9
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/opnfv_tests/sdn/odl/odl.py
+++ functest/opnfv_tests/sdn/odl/odl.py
@@ -165,10 +165,10 @@ class ODLTests(robotframework.RobotFramework):
                     'service_id': neutron_id})[0].url
             kwargs = {'neutronurl': endpoint}
             kwargs['odlip'] = env.get('SDN_CONTROLLER_IP')
-            kwargs['odlwebport'] = '8080'
-            kwargs['odlrestconfport'] = '8181'
-            kwargs['odlusername'] = 'admin'
-            kwargs['odlpassword'] = 'admin'
+            kwargs['odlwebport'] = env.get('SDN_CONTROLLER_WEBPORT')
+            kwargs['odlrestconfport'] = env.get('SDN_CONTROLLER_RESTCONFPORT')
+            kwargs['odlusername'] = env.get('SDN_CONTROLLER_USER')
+            kwargs['odlpassword'] = env.get('SDN_CONTROLLER_PASSWORD')
             installer_type = env.get('INSTALLER_TYPE')
             kwargs['osusername'] = os.environ['OS_USERNAME']
             kwargs['osuserdomainname'] = os.environ.get(
