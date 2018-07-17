ae315adbf7abd9166e3192727f302b137300439e
Cédric Ollivier <cedric.ollivier@orange.com>
2018-07-17 13:19:54 +0200

    Leverage cloudify_ims on Cloudify
    It also defines 2 new methods to avoid duplicating code:
      - TenantNetwork1.get_public_auth_url()
      - VmReady1.publish_image_alt()
    
    Shaker and Tempest are updated as well.
    
    Change-Id: Ie9095c57c580b95f067d5a8275ad71818b4df335
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit 8e96ddfb6a6548a9a7a9b4a686e01378425a1cd8)

--- functest/tests/unit/vnf/ims/test_cloudify_ims.py
+++ functest/tests/unit/vnf/ims/test_cloudify_ims.py
@@ -10,56 +10,9 @@
 import logging
 import unittest
 
-import mock
-
-from functest.core import vnf
-from functest.opnfv_tests.vnf.ims import cloudify_ims
-
 
 class CloudifyImsTesting(unittest.TestCase):
-
-    def setUp(self):
-
-        self.tenant = 'cloudify_ims'
-        self.creds = {'username': 'user',
-                      'password': 'pwd'}
-        self.orchestrator = {'name': 'cloudify',
-                             'version': '4.0',
-                             'object': 'foo',
-                             'requirements': {'flavor': {'name': 'm1.medium',
-                                                         'ram_min': 4096},
-                                              'os_image': 'manager_4.0'}}
-
-        self.vnf = {'name': 'clearwater',
-                    'descriptor': {'version': '108',
-                                   'file_name': 'openstack-blueprint.yaml',
-                                   'name': 'clearwater-opnfv',
-                                   'url': 'https://foo',
-                                   'requirements': {'flavor':
-                                                    {'name': 'm1.medium',
-                                                     'ram_min': 2048}}}}
-
-        with mock.patch('functest.opnfv_tests.vnf.ims.cloudify_ims.'
-                        'os.makedirs'), \
-            mock.patch('functest.opnfv_tests.vnf.ims.cloudify_ims.'
-                       'get_config', return_value={
-                           'tenant_images': 'foo',
-                           'orchestrator': self.orchestrator,
-                           'vnf': self.vnf,
-                           'vnf_test_suite': '',
-                           'version': 'whatever'}):
-
-            self.ims_vnf = cloudify_ims.CloudifyIms()
-
-        self.images = {'image1': 'url1',
-                       'image2': 'url2'}
-        self.details = {'orchestrator': {'status': 'PASS', 'duration': 120},
-                        'vnf': {},
-                        'test_vnf':  {}}
-
-    def test_prepare_missing_param(self):
-        with self.assertRaises(vnf.VnfPreparationException):
-            self.ims_vnf.prepare()
+    pass
 
 
 if __name__ == "__main__":
