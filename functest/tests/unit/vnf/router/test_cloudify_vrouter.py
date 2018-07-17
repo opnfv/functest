770ba9446a8fb292aa4f6e45a02cee61b3e7cd3a
Cédric Ollivier <cedric.ollivier@orange.com>
2018-07-17 18:00:37 +0200

    Leverage vyos_vrouter on Cloudify
    It also stop rebooting the VMs.
    
    Change-Id: Ib0f9e1aac1dd8505378b0688930eb41a590a11f1
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit e2fe0591bcfc462a91c028a857d967f4b9832bbe)

--- functest/tests/unit/vnf/router/test_cloudify_vrouter.py
+++ functest/tests/unit/vnf/router/test_cloudify_vrouter.py
@@ -12,61 +12,10 @@
 import logging
 import unittest
 
-import mock
-
-from functest.core import vnf
-from functest.opnfv_tests.vnf.router import cloudify_vrouter
-
 
 class CloudifyVrouterTesting(unittest.TestCase):
 
-    def setUp(self):
-
-        self.tenant = 'cloudify_vrouter'
-        self.creds = {'username': 'user',
-                      'password': 'pwd'}
-        self.orchestrator = {'name': 'cloudify',
-                             'version': '4.0',
-                             'object': 'foo',
-                             'requirements': {'flavor': {'name': 'm1.medium',
-                                                         'ram_min': 4096},
-                                              'os_image': 'manager_4.0'}}
-
-        self.vnf = {'name': 'vrouter',
-                    'descriptor': {'version': '100',
-                                   'file_name': 'function-test-' +
-                                                'openstack-blueprint.yaml',
-                                   'name': 'vrouter-opnfv',
-                                   'url': 'https://foo',
-                                   'requirements': {'flavor':
-                                                    {'name': 'm1.medium',
-                                                     'ram_min': 2048}}}}
-
-        # pylint: disable=bad-continuation
-        with mock.patch(
-                'functest.opnfv_tests.vnf.router.cloudify_vrouter.Utilvnf'), \
-                mock.patch('functest.opnfv_tests.vnf.router.'
-                           'cloudify_vrouter.vrouter_base.Utilvnf'), \
-                mock.patch('os.makedirs'), \
-                mock.patch(
-                    'functest.utils.functest_utils.get_parameter_from_yaml',
-                    return_value={
-                        'tenant_images': 'foo',
-                        'orchestrator': self.orchestrator,
-                        'vnf': self.vnf, 'vnf_test_suite': '',
-                        'version': 'whatever'}):
-
-            self.router_vnf = cloudify_vrouter.CloudifyVrouter()
-
-        self.images = {'image1': 'url1',
-                       'image2': 'url2'}
-        self.details = {'orchestrator': {'status': 'PASS', 'duration': 120},
-                        'vnf': {},
-                        'test_vnf':  {}}
-
-    def test_prepare_missing_param(self):
-        with self.assertRaises(vnf.VnfPreparationException):
-            self.router_vnf.prepare()
+    pass
 
 
 if __name__ == "__main__":
