c8dffa3b40d7893361d1ac303b340d3b9b1b76a6
Cédric Ollivier <cedric.ollivier@orange.com>
2018-08-18 08:09:53 +0200

    Publish Functest doc online
    It reverts Sphinx theme to the default one as it's hugely better for
    API docs.
    
    Change-Id: Ic489e9b65d67d71fadb8ef78176a04cc382a3f5d
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit 20ee61db8d38bf12127afd12e34ac601d36b3ff5)

--- docs/testing/developer/internship/security_group/conf.py
+++ docs/testing/developer/internship/security_group/conf.py
@@ -19,8 +19,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_opnfv_theme
-
 
 # -- General configuration ------------------------------------------------
 
@@ -83,22 +81,16 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'opnfv'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 # html_theme_options = {}
-html_theme_options = {
-    'bootswatch_theme': 'journal',
-    'navbar_sidebarrel': False,
-    'navbar_title': '',
-}
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
-html_theme_path = sphinx_opnfv_theme.get_html_theme_path()
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
