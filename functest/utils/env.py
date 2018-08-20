5da5902733a063004e241442acf2dabc9a74c0da
Stamatis Katsaounis <mokats@intracom-telecom.com>
2018-08-20 11:58:20 +0300

    Run Intel NFV CI test-cases inside functest smoke.
    JIRA: FUNCTEST-1004
    
    This patch integrates tempest test-cases from Intel NFV CI
    tempest plugin (https://github.com/openstack/intel-nfv-ci-tests).
    
    Change-Id: I6ab549cdcb3d081c1df9e173b579c86abc8a1427
    Signed-off-by: Stamatis Katsaounis <mokats@intracom-telecom.com>

--- functest/utils/env.py
+++ functest/utils/env.py
@@ -35,7 +35,9 @@ INPUTS = {
     'NAMESERVER': '8.8.8.8',
     'NEW_USER_ROLE': 'Member',
     'USE_DYNAMIC_CREDENTIALS': 'True',
-    'STORAGE_PROTOCOL': 'iSCSI'
+    'STORAGE_PROTOCOL': 'iSCSI',
+    'LIBVIRT_USER': None,
+    'LIBVIRT_KEY_PATH': None
 }
 
 
