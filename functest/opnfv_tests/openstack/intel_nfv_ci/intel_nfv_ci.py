0fd46c18d43e5d9ff1def9cab6e6ec866da4e8cc
Stamatis Katsaounis <mokats@intracom-telecom.com>
2018-08-20 11:58:20 +0300

    Run Intel NFV CI test-cases inside functest smoke.
    JIRA: FUNCTEST-1004
    
    This patch integrates tempest test-cases from Intel NFV CI
    tempest plugin (https://github.com/openstack/intel-nfv-ci-tests).
    
    Change-Id: I6ab549cdcb3d081c1df9e173b579c86abc8a1427
    Signed-off-by: Stamatis Katsaounis <mokats@intracom-telecom.com>

---
+++ functest/opnfv_tests/openstack/intel_nfv_ci/intel_nfv_ci.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2018 Intracom Telecom and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# pylint: disable=missing-docstring
+
+import logging
+
+from six.moves import configparser
+
+from functest.opnfv_tests.openstack.tempest import tempest
+
+
+class IntelNfvCi(tempest.TempestCommon):
+
+    __logger = logging.getLogger(__name__)
+
+    def configure(self, **kwargs):
+        super(IntelNfvCi, self).configure(**kwargs)
+        rconfig = configparser.RawConfigParser()
+        rconfig.read(self.conf_file)
+        rconfig.add_section('intel_nfv_ci')
+        rconfig.set('intel_nfv_ci', 'qemu_ssh_user',
+                    kwargs.get('qemu_ssh_user', 'ubuntu'))
+        rconfig.set('intel_nfv_ci', 'qemu_ssh_private_key_path',
+                    kwargs.get('qemu_ssh_private_key_path',
+                               '/root/.ssh/id_rsa'))
+        with open(self.conf_file, 'wb') as config_file:
+            rconfig.write(config_file)
+        self.backup_tempest_config(self.conf_file, self.res_dir)
