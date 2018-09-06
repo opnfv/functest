41a4ee5170d6f46042d0ba365d78a21be6ec5b1b
Delia Popescu <delia.popescu@enea.com>
2018-09-06 18:37:11 +0300

    Use environment variable for image properties
    Use IMG_PROP environment variable to read extra image
    properties.
    
    Set image_extra_properties for scsi volume types
    without file patch, but using IMG_PROP env variable
    IMG_PROP is defined through releng.
    
    Image extra_properties will continue to get updates from
    functest config file
    
    With this change, remove unnecessary parameters from arm patch
    Related to:https://gerrit.opnfv.org/gerrit/#/c/63203/
    
    JIRA:ARMBAND-402
    
    Change-Id: I64139240751b1e109bbe24ba32f487ccd98b9a92
    Signed-off-by: Delia Popescu <delia.popescu@enea.com>
    (cherry picked from commit d7c5419ed8d3dcdb59f4f5f254de0c7bb7a0aec6)

--- functest/core/singlevm.py
+++ functest/core/singlevm.py
@@ -23,6 +23,7 @@ from xtesting.core import testcase
 
 from functest.core import tenantnetwork
 from functest.utils import config
+from functest.utils import env
 
 
 class VmReady1(tenantnetwork.TenantNetwork1):
@@ -73,6 +74,10 @@ class VmReady1(tenantnetwork.TenantNetwork1):
         """
         assert self.cloud
         extra_properties = self.extra_properties.copy()
+        if env.get('IMG_PROP'):
+            extra_properties.update(dict((k.strip(), v.strip()) for k, v in
+                                         (item.split(': ') for item in
+                                          env.get('IMG_PROP').split(','))))
         extra_properties.update(
             getattr(config.CONF, '{}_extra_properties'.format(
                 self.case_name), {}))
@@ -104,6 +109,10 @@ class VmReady1(tenantnetwork.TenantNetwork1):
         """
         assert self.cloud
         extra_alt_properties = self.extra_alt_properties.copy()
+        if env.get('IMG_PROP'):
+            extra_alt_properties.update(dict((k.strip(), v.strip()) for k, v in
+                                             (item.split(': ') for item in
+                                              env.get('IMG_PROP').split(','))))
         extra_alt_properties.update(
             getattr(config.CONF, '{}_extra_alt_properties'.format(
                 self.case_name), {}))
