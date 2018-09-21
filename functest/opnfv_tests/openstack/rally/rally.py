9b5fd7be656b2887b57d5f74663c04b464fad821
Cédric Ollivier <cedric.ollivier@orange.com>
2018-09-21 17:25:44 +0200

    Remove rally plugins and extra when cleaning
    Change-Id: I15393b870d648432c900d429d17129c630ed4f58
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit aff6640b5aed37221a05067e93ee164273c16910)

--- functest/opnfv_tests/openstack/rally/rally.py
+++ functest/opnfv_tests/openstack/rally/rally.py
@@ -640,16 +640,21 @@ class RallyJobs(RallyBase):
         with open(result_file_name, 'w') as fname:
             template.dump(cases, fname)
 
-    def prepare_task(self, test_name):
-        """Prepare resources for test run."""
+    @staticmethod
+    def _remove_plugins_extra():
         inst_dir = getattr(config.CONF, 'dir_rally_inst')
         try:
             shutil.rmtree(os.path.join(inst_dir, 'plugins'))
             shutil.rmtree(os.path.join(inst_dir, 'extra'))
         except Exception:  # pylint: disable=broad-except
             pass
+
+    def prepare_task(self, test_name):
+        """Prepare resources for test run."""
+        self._remove_plugins_extra()
         jobs_dir = os.path.join(
             getattr(config.CONF, 'dir_rally_data'), test_name, 'rally-jobs')
+        inst_dir = getattr(config.CONF, 'dir_rally_inst')
         shutil.copytree(os.path.join(jobs_dir, 'plugins'),
                         os.path.join(inst_dir, 'plugins'))
         shutil.copytree(os.path.join(jobs_dir, 'extra'),
@@ -667,3 +672,7 @@ class RallyJobs(RallyBase):
         self.apply_blacklist(task, task_file_name)
         self.run_cmd = (["rally", "task", "start", "--task", task_file_name])
         return True
+
+    def clean(self):
+        self._remove_plugins_extra()
+        super(RallyJobs, self).clean()
