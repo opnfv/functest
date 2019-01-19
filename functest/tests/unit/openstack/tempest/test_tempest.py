b482b494ce695e83cca0a10e7750200fd1be42ea
Cédric Ollivier <cedric.ollivier@orange.com>
2019-01-19 16:19:25 +0100

    Move logics out of TempestCommon.__init__()
    It conforms with best programming practices.
    It allows muting console and increasing log level (debug).
    
    Change-Id: I0e0cfef6abe3b354a43817fcb242c71da701e237
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/tests/unit/openstack/tempest/test_tempest.py
+++ functest/tests/unit/openstack/tempest/test_tempest.py
@@ -82,6 +82,7 @@ class OSTempestTesting(unittest.TestCase):
         else:
             testr_mode = self.tempestcommon.mode
         verifier_repo_dir = 'test_verifier_repo_dir'
+        self.tempestcommon.verifier_repo_dir = verifier_repo_dir
         cmd = "(cd {0}; stestr list '{1}' >{2} 2>/dev/null)".format(
             verifier_repo_dir, testr_mode, self.tempestcommon.list)
         self.tempestcommon.generate_test_list(mode=testr_mode)
