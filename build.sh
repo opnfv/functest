0a897d6edd561976552196572ee843f9fdae4f31
Cédric Ollivier <cedric.ollivier@orange.com>
2019-03-28 20:10:49 +0100

    Prepare Functest Iruya Containers
    It takes into account the new branch stable/iruya.
    
    Change-Id: I802a42b07ceae2328a812a99a99057beb1b94e5b
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -15,36 +15,36 @@ arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:amd64-latest|g" {} +
+    -e "s|opnfv/functest-core:iruya|${repo}/functest-core:amd64-iruya|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:amd64-latest|g" {} +
+    -e "s|opnfv/functest-tempest:iruya|${repo}/functest-tempest:amd64-iruya|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
         docker build "${build_opts[@]}" \
-            -t "${repo}/functest-${dir##**/}:amd64-latest" .)
-        docker push "${repo}/functest-${dir##**/}:amd64-latest"
+            -t "${repo}/functest-${dir##**/}:amd64-iruya" .)
+        docker push "${repo}/functest-${dir##**/}:amd64-iruya"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:amd64-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:amd64-iruya" || true)
 done
 [ ! -z "${amd64_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:amd64-latest" alpine:3.9 || true)
+    (docker rmi "${repo}/functest-core:amd64-iruya" alpine:3.9 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.9|multiarch/alpine:arm64-v3.9|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:arm64-latest|g" {} +
+    -e "s|opnfv/functest-core:iruya|${repo}/functest-core:arm64-iruya|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm64-latest|g" {} +
+    -e "s|opnfv/functest-tempest:iruya|${repo}/functest-tempest:arm64-iruya|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-${dir##**/}:arm64-latest" .)
-    docker push "${repo}/functest-${dir##**/}:arm64-latest"
+        -t "${repo}/functest-${dir##**/}:arm64-iruya" .)
+    docker push "${repo}/functest-${dir##**/}:arm64-iruya"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:arm64-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:arm64-iruya" || true)
 done
 [ ! -z "${arm64_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:arm64-latest" \
+    (docker rmi "${repo}/functest-core:arm64-iruya" \
         multiarch/alpine:arm64-v3.9 || true)
 find . -name Dockerfile -exec git checkout {} +
 
