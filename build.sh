5f525ebaf9d503aa641996fe2ac780dc9dda1774
Cédric Ollivier <cedric.ollivier@orange.com>
2019-04-13 12:39:57 +0200

    Cross-compile arm and arm64 images
    It allows building images for Raspberry PI via travis-ci.
    It builds all arm64 images except functest-benchmarking [1].
    
    [1] https://travis-ci.org/collivier/functest/jobs/519673994
    
    Change-Id: I695c3afe8aacda63b7eae0d2672cd836c19e8ceb
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -11,6 +11,13 @@ docker/smoke \
 docker/benchmarking \
 docker/features \
 docker/vnf"}
+arm_dirs=${arm_dirs-"\
+docker/core \
+docker/tempest \
+docker/healthcheck \
+docker/smoke \
+docker/benchmarking \
+docker/features"}
 arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=(--pull=true --no-cache --force-rm=true)
 
@@ -46,6 +53,24 @@ done
 [ ! -z "${arm64_dirs}" ] &&
     (docker rmi "${repo}/functest-core:arm64-latest" \
         multiarch/alpine:arm64-v3.9 || true)
+find . -name Dockerfile -exec git checkout {} +
+
+find . -name Dockerfile -exec sed -i \
+    -e "s|alpine:3.9|multiarch/alpine:armhf-v3.9|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-core|${repo}/functest-core:arm-latest|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm-latest|g" {} +
+for dir in ${arm_dirs}; do
+    (cd "${dir}" && docker build "${build_opts[@]}" \
+        -t "${repo}/functest-${dir##**/}:arm-latest" .)
+    docker push "${repo}/functest-${dir##**/}:arm-latest"
+    [ "${dir}" != "docker/core" ] &&
+        (docker rmi "${repo}/functest-${dir##**/}:arm-latest" || true)
+done
+[ ! -z "${arm_dirs}" ] &&
+    (docker rmi "${repo}/functest-core:arm-latest" \
+        multiarch/alpine:armhf-v3.9 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 exit $?
