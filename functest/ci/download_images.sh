ef0431498920058768361cf908aa79d8e645ef4b
Cédric Ollivier <cedric.ollivier@orange.com>
2020-04-02 18:37:30 +0200

    Add tempest_heat (heat-tempest-plugin)
    It asks for the Fedora-Cloud-Base image and a second user account.
    It should be noted that heat-tempest-plugin duplicates all configs from
    tempest.
    
    Minimal image is still Cirros and the first account is the admin one.
    Then it conforms with the TempestCommon logic.
    
    AodhAlarmTest is skipped by default because pre conditions are missing.
    SoftwareConfigIntegrationTest is skipped as in gates because it
    requires a custom image [1]
    
    [1] https://github.com/openstack/heat/blob/master/devstack/lib/heat#L444
    
    Change-Id: I8b39dc65ef3714411cd828e17b95e124f2e90f34
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/ci/download_images.sh
+++ functest/ci/download_images.sh
@@ -14,4 +14,5 @@ https://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloud
 http://repository.cloudifysource.org/cloudify/19.01.24/community-release/cloudify-docker-manager-community-19.01.24.tar
 http://testresults.opnfv.org/functest/vyos-1.1.8-amd64.qcow2
 http://testresults.opnfv.org/functest/shaker-image-1.3.0+stretch.qcow2
+https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/30/Cloud/x86_64/images/Fedora-Cloud-Base-30-1.2.x86_64.qcow2
 EOF
