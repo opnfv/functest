d83fbefdef047c10170ab53e4671a882372d08f8
Cédric Ollivier <cedric.ollivier@orange.com>
2020-04-28 21:18:31 +0200

    Fix sed in build.sh
    Change-Id: I3f7e51b869d13b109699d17547e28754fc1392dd
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -22,9 +22,7 @@ arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=("--pull=true" --no-cache "--force-rm=true")
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:amd64-kali|g" {} +
-find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:amd64-kali|g" {} +
+    -e "s|opnfv/functest-core:kali|${repo}/functest-core:amd64-kali|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
         docker build "${build_opts[@]}" \
@@ -40,9 +38,7 @@ find . -name Dockerfile -exec git checkout {} +
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.11|arm64v8/alpine:3.11|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:arm64-kali|g" {} +
-find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm64-kali|g" {} +
+    -e "s|opnfv/functest-core:kali|${repo}/functest-core:arm64-kali|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
         -t "${repo}/functest-${dir##**/}:arm64-kali" .)
@@ -58,9 +54,7 @@ find . -name Dockerfile -exec git checkout {} +
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.11|arm32v6/alpine:3.11|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:arm-kali|g" {} +
-find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm-kali|g" {} +
+    -e "s|opnfv/functest-core:kali|${repo}/functest-core:arm-kali|g" {} +
 for dir in ${arm_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
         -t "${repo}/functest-${dir##**/}:arm-kali" .)
