196715daf5c802a22025ed24531ba09793ee9c12
Cédric Ollivier <cedric.ollivier@orange.com>
2020-04-28 15:58:00 +0200

    Publish Functest Kali containers
    Change-Id: I5cfc63a7a5b1434bccce564d3e17a4dd6c7a424e
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -22,54 +22,54 @@ arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=("--pull=true" --no-cache "--force-rm=true")
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:amd64-latest|g" {} +
+    -e "s|opnfv/functest-core|${repo}/functest-core:amd64-kali|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:amd64-latest|g" {} +
+    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:amd64-kali|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
         docker build "${build_opts[@]}" \
-            -t "${repo}/functest-${dir##**/}:amd64-latest" .)
-        docker push "${repo}/functest-${dir##**/}:amd64-latest"
+            -t "${repo}/functest-${dir##**/}:amd64-kali" .)
+        docker push "${repo}/functest-${dir##**/}:amd64-kali"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:amd64-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:amd64-kali" || true)
 done
 [ -n "${amd64_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:amd64-latest" alpine:3.11 || true)
+    (docker rmi "${repo}/functest-core:amd64-kali" alpine:3.11 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.11|arm64v8/alpine:3.11|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:arm64-latest|g" {} +
+    -e "s|opnfv/functest-core|${repo}/functest-core:arm64-kali|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm64-latest|g" {} +
+    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm64-kali|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-${dir##**/}:arm64-latest" .)
-    docker push "${repo}/functest-${dir##**/}:arm64-latest"
+        -t "${repo}/functest-${dir##**/}:arm64-kali" .)
+    docker push "${repo}/functest-${dir##**/}:arm64-kali"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:arm64-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:arm64-kali" || true)
 done
 [ -n "${arm64_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:arm64-latest" \
+    (docker rmi "${repo}/functest-core:arm64-kali" \
         arm64v8/alpine:3.11 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.11|arm32v6/alpine:3.11|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:arm-latest|g" {} +
+    -e "s|opnfv/functest-core|${repo}/functest-core:arm-kali|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm-latest|g" {} +
+    -e "s|opnfv/functest-tempest|${repo}/functest-tempest:arm-kali|g" {} +
 for dir in ${arm_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-${dir##**/}:arm-latest" .)
-    docker push "${repo}/functest-${dir##**/}:arm-latest"
+        -t "${repo}/functest-${dir##**/}:arm-kali" .)
+    docker push "${repo}/functest-${dir##**/}:arm-kali"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:arm-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:arm-kali" || true)
 done
 [ -n "${arm_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:arm-latest" \
+    (docker rmi "${repo}/functest-core:arm-kali" \
         arm32v6/alpine:3.11 || true)
 find . -name Dockerfile -exec git checkout {} +
 
