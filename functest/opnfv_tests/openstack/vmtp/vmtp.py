8b02053577c6668324aacb6d8a0ded3e728f0a42
Cédric Ollivier <cedric.ollivier@orange.com>
2020-06-29 13:36:43 +0200

    Simplify hypervisor list logic in vmtp
    It completes the previous change "Allow listing hypervisors in VMTP" [1].
    
    [1] https://gerrit.opnfv.org/gerrit/c/functest/+/70359
    
    Change-Id: I12a19fd473076cfc3262f096cac1615c183710b1
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit 06f69562e0937535e665840336b951dbed7ebd1b)

--- functest/opnfv_tests/openstack/vmtp/vmtp.py
+++ functest/opnfv_tests/openstack/vmtp/vmtp.py
@@ -149,15 +149,14 @@ class Vmtp(singlevm.VmReady2):
             del new_env['OS_TENANT_ID']
         except Exception:  # pylint: disable=broad-except
             pass
-        extra_args = ""
+        cmd = ['vmtp', '-d', '--json', '{}/vmtp.json'.format(self.res_dir),
+               '-c', self.config]
         if env.get("VMTP_HYPERVISORS"):
             hypervisors = functest_utils.convert_ini_to_list(
                 env.get("VMTP_HYPERVISORS"))
             for hypervisor in hypervisors:
-                extra_args = "{} --hypervisor {} ".format(
-                    extra_args, hypervisor)
-        cmd = ['vmtp', '-d', '--json', '{}/vmtp.json'.format(self.res_dir),
-               '-c', self.config, extra_args]
+                cmd.extend(["--hypervisor", hypervisor])
+        self.__logger.debug("cmd: %s", cmd)
         output = subprocess.check_output(
             cmd, stderr=subprocess.STDOUT, env=new_env).decode("utf-8")
         self.__logger.info("%s\n%s", " ".join(cmd), output)
