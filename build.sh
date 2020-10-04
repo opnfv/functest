d69581e43981d0a5f85ca044cc191f31169b55ab
Cédric Ollivier <cedric.ollivier@orange.com>
2020-10-04 16:20:27 +0200

    Modify smoke and benchmarking in Docker From
    Change-Id: I0d7acd82f3a4305e862e924d1199c5d28d3ccf56
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>
    (cherry picked from commit e59aaac6625fcc78de636c90d0738aaf899f2757)

--- build.sh
+++ build.sh
@@ -17,6 +17,11 @@ build_opts=("--pull=true" --no-cache "--force-rm=true")
 
 find . -name Dockerfile -exec sed -i \
     -e "s|opnfv/functest-core:kali|${repo}/functest-core:amd64-kali|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-smoke:kali|${repo}/functest-smoke:amd64-kali|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-benchmarking:kali|\
+${repo}/functest-benchmarking:amd64-kali|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
         docker build "${build_opts[@]}" \
@@ -33,6 +38,11 @@ find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.11|arm64v8/alpine:3.11|g" {} +
 find . -name Dockerfile -exec sed -i \
     -e "s|opnfv/functest-core:kali|${repo}/functest-core:arm64-kali|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-smoke:kali|${repo}/functest-smoke:arm64-kali|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-benchmarking:kali|\
+${repo}/functest-benchmarking:arm64-kali|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
         -t "${repo}/functest-${dir##**/}:arm64-kali" .)
@@ -49,6 +59,11 @@ find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.11|arm32v6/alpine:3.11|g" {} +
 find . -name Dockerfile -exec sed -i \
     -e "s|opnfv/functest-core:kali|${repo}/functest-core:arm-kali|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-smoke:kali|${repo}/functest-smoke:arm-kali|g" {} +
+find . -name Dockerfile -exec sed -i \
+    -e "s|opnfv/functest-benchmarking:kali|\
+${repo}/functest-benchmarking:arm-kali|g" {} +
 for dir in ${arm_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
         -t "${repo}/functest-${dir##**/}:arm-kali" .)
