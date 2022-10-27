36a3003a95b2d67cef1810279131fcda97120f30
Cédric Ollivier <cedric.ollivier@orange.com>
2022-10-27 13:55:22 +0200

    Set software releases
    Change-Id: I96a0bc1ac468a982d5013ec081528e05d94d3053
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -16,63 +16,63 @@ arm64_dirs=${arm64_dirs-${amd64_dirs}}
 build_opts=("--pull=true" --no-cache "--force-rm=true")
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:amd64-latest|g" {} +
+    -e "s|opnfv/functest-core:zed|${repo}/functest-core:amd64-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-smoke|${repo}/functest-smoke:amd64-latest|g" {} +
+    -e "s|opnfv/functest-smoke:zed|${repo}/functest-smoke:amd64-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-benchmarking|\
-${repo}/functest-benchmarking:amd64-latest|g" {} +
+    -e "s|opnfv/functest-benchmarking:zed|\
+${repo}/functest-benchmarking:amd64-zed|g" {} +
 for dir in ${amd64_dirs}; do
     (cd "${dir}" &&
         docker build "${build_opts[@]}" \
-            -t "${repo}/functest-${dir##**/}:amd64-latest" .)
-        docker push "${repo}/functest-${dir##**/}:amd64-latest"
+            -t "${repo}/functest-${dir##**/}:amd64-zed" .)
+        docker push "${repo}/functest-${dir##**/}:amd64-zed"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:amd64-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:amd64-zed" || true)
 done
 [ -n "${amd64_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:amd64-latest" alpine:3.14 || true)
+    (docker rmi "${repo}/functest-core:amd64-zed" alpine:3.14 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.14|arm64v8/alpine:3.14|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:arm64-latest|g" {} +
+    -e "s|opnfv/functest-core:zed|${repo}/functest-core:arm64-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-smoke|${repo}/functest-smoke:arm64-latest|g" {} +
+    -e "s|opnfv/functest-smoke:zed|${repo}/functest-smoke:arm64-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-benchmarking|\
-${repo}/functest-benchmarking:arm64-latest|g" {} +
+    -e "s|opnfv/functest-benchmarking:zed|\
+${repo}/functest-benchmarking:arm64-zed|g" {} +
 for dir in ${arm64_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-${dir##**/}:arm64-latest" .)
-    docker push "${repo}/functest-${dir##**/}:arm64-latest"
+        -t "${repo}/functest-${dir##**/}:arm64-zed" .)
+    docker push "${repo}/functest-${dir##**/}:arm64-zed"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:arm64-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:arm64-zed" || true)
 done
 [ -n "${arm64_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:arm64-latest" \
+    (docker rmi "${repo}/functest-core:arm64-zed" \
         arm64v8/alpine:3.14 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
     -e "s|alpine:3.14|arm32v6/alpine:3.14|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-core|${repo}/functest-core:arm-latest|g" {} +
+    -e "s|opnfv/functest-core:zed|${repo}/functest-core:arm-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-smoke|${repo}/functest-smoke:arm-latest|g" {} +
+    -e "s|opnfv/functest-smoke:zed|${repo}/functest-smoke:arm-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
-    -e "s|opnfv/functest-benchmarking|\
-${repo}/functest-benchmarking:arm-latest|g" {} +
+    -e "s|opnfv/functest-benchmarking:zed|\
+${repo}/functest-benchmarking:arm-zed|g" {} +
 for dir in ${arm_dirs}; do
     (cd "${dir}" && docker build "${build_opts[@]}" \
-        -t "${repo}/functest-${dir##**/}:arm-latest" .)
-    docker push "${repo}/functest-${dir##**/}:arm-latest"
+        -t "${repo}/functest-${dir##**/}:arm-zed" .)
+    docker push "${repo}/functest-${dir##**/}:arm-zed"
     [ "${dir}" != "docker/core" ] &&
-        (docker rmi "${repo}/functest-${dir##**/}:arm-latest" || true)
+        (docker rmi "${repo}/functest-${dir##**/}:arm-zed" || true)
 done
 [ -n "${arm_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:arm-latest" \
+    (docker rmi "${repo}/functest-core:arm-zed" \
         arm32v6/alpine:3.14 || true)
 find . -name Dockerfile -exec git checkout {} +
 
