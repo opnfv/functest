d6cef9def55ebe92bed40af44dd58b005c31f517
Cédric Ollivier <cedric.ollivier@orange.com>
2022-11-02 14:48:41 +0100

    Update Cirros for testing
    It avoids errors when reading ssh banneer.
    
    Change-Id: I67ba57f56914359e488936873cd2ce639a839b52
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- functest/ci/download_images.sh
+++ functest/ci/download_images.sh
@@ -6,10 +6,10 @@ wget_opts="-N --tries=1 --connect-timeout=30"
 [ -t 1 ] || wget_opts="${wget_opts} --progress=dot:giga"
 
 cat << EOF  | wget ${wget_opts} -i - -P ${1:-/home/opnfv/functest/images}
-http://download.cirros-cloud.net/0.5.1/cirros-0.5.1-x86_64-disk.img
+http://download.cirros-cloud.net/0.6.0/cirros-0.6.0-x86_64-disk.img
 https://cloud-images.ubuntu.com/releases/14.04/release/ubuntu-14.04-server-cloudimg-amd64-disk1.img
 https://cloud-images.ubuntu.com/releases/16.04/release/ubuntu-16.04-server-cloudimg-amd64-disk1.img
-http://download.cirros-cloud.net/0.5.1/cirros-0.5.1-aarch64-disk.img
+http://download.cirros-cloud.net/0.6.0/cirros-0.6.0-aarch64-disk.img
 http://repository.cloudifysource.org/cloudify/19.01.24/community-release/cloudify-docker-manager-community-19.01.24.tar
 http://testresults.opnfv.org/functest/vyos-1.1.8-amd64.qcow2
 http://testresults.opnfv.org/functest/shaker-image-1.3.4+stretch.qcow2
