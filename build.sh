1aa55fbdfe0271664cf20ce9c2e7ad579901502f
Cédric Ollivier <cedric.ollivier@orange.com>
2023-03-06 13:02:21 +0100

    Upgrade to Alpine 3.17
    Change-Id: Ia9d64a094e787e0693fab3c59deb33e2404ccfc6
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- build.sh
+++ build.sh
@@ -31,11 +31,11 @@ for dir in ${amd64_dirs}; do
         (docker rmi "${repo}/functest-${dir##**/}:amd64-zed" || true)
 done
 [ -n "${amd64_dirs}" ] &&
-    (docker rmi "${repo}/functest-core:amd64-zed" alpine:3.14 || true)
+    (docker rmi "${repo}/functest-core:amd64-zed" alpine:3.17 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|alpine:3.14|arm64v8/alpine:3.14|g" {} +
+    -e "s|alpine:3.17|arm64v8/alpine:3.17|g" {} +
 find . -name Dockerfile -exec sed -i \
     -e "s|opnfv/functest-core:zed|${repo}/functest-core:arm64-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
@@ -52,11 +52,11 @@ for dir in ${arm64_dirs}; do
 done
 [ -n "${arm64_dirs}" ] &&
     (docker rmi "${repo}/functest-core:arm64-zed" \
-        arm64v8/alpine:3.14 || true)
+        arm64v8/alpine:3.17 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 find . -name Dockerfile -exec sed -i \
-    -e "s|alpine:3.14|arm32v6/alpine:3.14|g" {} +
+    -e "s|alpine:3.17|arm32v6/alpine:3.17|g" {} +
 find . -name Dockerfile -exec sed -i \
     -e "s|opnfv/functest-core:zed|${repo}/functest-core:arm-zed|g" {} +
 find . -name Dockerfile -exec sed -i \
@@ -73,7 +73,7 @@ for dir in ${arm_dirs}; do
 done
 [ -n "${arm_dirs}" ] &&
     (docker rmi "${repo}/functest-core:arm-zed" \
-        arm32v6/alpine:3.14 || true)
+        arm32v6/alpine:3.17 || true)
 find . -name Dockerfile -exec git checkout {} +
 
 exit $?
